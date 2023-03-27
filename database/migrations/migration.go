0eb23fef2bf3cacfda6d4e834b1f8ddae7e4a6d8
arfanxn <arfan2173@gmail.com>
2023-03-28 00:30:00 +0700

    add migration

---
+++ database/migrations/migration.go
@@ -0,0 +1,20 @@
+package migrations
+
+import (
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	conn "github.com/arfanxn/coursefan-gofiber/database/connection"
+)
+
+func Migrate() error {
+	db, err := conn.GetGORM()
+	if err != nil {
+		return err
+	}
+
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
