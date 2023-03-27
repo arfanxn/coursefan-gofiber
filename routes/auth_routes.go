65716b84d2339107063e2fb43a8e976a709711c6
arfanxn <arfan2173@gmail.com>
2023-03-28 00:29:37 +0700

    update route

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -1,7 +1,13 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/arfanxn/coursefan-gofiber/app/http/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func registerAuthRoutes(router fiber.Router) error {
+	authController := controllers.NewAuthController()
+	router.Post("/login", authController.Login)
+
 	return nil
 }
