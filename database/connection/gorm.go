fe21361a04aa859435540b57eb34e80092f52027
arfanxn <arfan2173@gmail.com>
2023-03-27 18:05:41 +0700

    add gorm

---
+++ database/connection/gorm.go
@@ -0,0 +1,40 @@
+package conn
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	gormDB *gorm.DB
+)
+
+func InitGORM() (*gorm.DB, error) {
+	database := os.Getenv("DB_DATABASE")
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+
+	config := &gorm.Config{}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(dsn), config)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func GetGORM() (*gorm.DB, error) {
+	if gormDB != nil {
+		return gormDB, nil
+	}
+	var err error
+	gormDB, err = InitGORM()
+	return gormDB, err
+}
