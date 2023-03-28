b59665e908b27146c924170ffec53957d868243f
arfanxn <arfan2173@gmail.com>
2023-03-29 01:07:37 +0700

    update err status code

--- app/exceptions/auth_exception.go
+++ app/exceptions/auth_exception.go
@@ -5,5 +5,5 @@ import "github.com/gofiber/fiber/v2"
 // Auth exceptions
 var (
 	AuthSessionExpired          = fiber.NewError(fiber.StatusUnauthorized, "Session expired please sign in")
-	AuthCredentialsDoesNotMatch = fiber.NewError(fiber.StatusUnauthorized, "Credentials does not match our records")
+	AuthCredentialsDoesNotMatch = fiber.NewError(fiber.StatusUnprocessableEntity, "Credentials does not match our records")
 )
