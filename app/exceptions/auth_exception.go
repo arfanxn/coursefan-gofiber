4bb8b5f8480c180b9b80899a38f673f7a895ab00
arfanxn <arfan2173@gmail.com>
2023-03-28 22:34:13 +0700

    add AuthCredentialsDoesNotMatch

--- app/exceptions/auth_exception.go
+++ app/exceptions/auth_exception.go
@@ -4,5 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 // Auth exceptions
 var (
-	AuthSessionExpired = fiber.NewError(fiber.StatusUnauthorized, "Session expired please sign in")
+	AuthSessionExpired          = fiber.NewError(fiber.StatusUnauthorized, "Session expired please sign in")
+	AuthCredentialsDoesNotMatch = fiber.NewError(fiber.StatusUnauthorized, "Credentials does not match our records")
 )
