3c2caa13e423e579ebcb71bd476a7208cb2c24fe
arfanxn <arfan2173@gmail.com>
2023-03-28 21:07:34 +0700

    make validator exceptions

--- app/exceptions/validator_exception.go
+++ app/exceptions/validator_exception.go
@@ -2,6 +2,21 @@ package exceptions
 
 import "github.com/gofiber/fiber/v2"
 
+type ValidatorError struct {
+	Field, Message string
+}
+
+func (validatorError *ValidatorError) Error() string {
+	return validatorError.Message
+}
+
+func NewValidatorError(field, message string) *ValidatorError {
+	return &ValidatorError{
+		Field:   field,
+		Message: message,
+	}
+}
+
 // Validator exceptions
 var (
 	ValidatorUnknownTranslator = fiber.NewError(fiber.StatusInternalServerError, "Validator unknown translator")
