4786552178d2c4860ae1b7790b3df4b5b9489960
arfanxn <arfan2173@gmail.com>
2023-03-28 21:56:32 +0700

    update wire

---
+++ app/providers/controllers/injector.go
@@ -0,0 +1,17 @@
+//go:build wireinject
+// +build wireinject
+
+package controllers
+
+import (
+	"github.com/arfanxn/coursefan-gofiber/app/http/controllers"
+	"github.com/arfanxn/coursefan-gofiber/app/repositories"
+	"github.com/arfanxn/coursefan-gofiber/app/services"
+	"github.com/google/wire"
+	"gorm.io/gorm"
+)
+
+func AuthController(db *gorm.DB) *controllers.AuthController {
+	wire.Build(repositories.NewUserRepository, services.NewAuthService, controllers.NewAuthController)
+	return nil
+}
