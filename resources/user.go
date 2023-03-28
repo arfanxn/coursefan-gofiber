31d7d942658d7355c10eed55afc5425c6c260f0a
arfanxn <arfan2173@gmail.com>
2023-03-28 16:36:09 +0700

    make user resource

---
+++ resources/user.go
@@ -0,0 +1,15 @@
+package resources
+
+import (
+	"time"
+
+	"gopkg.in/guregu/null.v4"
+)
+
+type User struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt null.Time `json:"updated_at"`
+}
