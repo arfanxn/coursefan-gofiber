0ff64edab9fc376c0b2414d3e1a2b9753530ef1f
arfanxn <arfan2173@gmail.com>
2023-03-29 21:39:21 +0700

    update reflect helper

--- app/helpers/reflecth/reflecth.go
+++ app/helpers/reflecth/reflecth.go
@@ -2,24 +2,6 @@ package reflecth
 
 import "reflect"
 
-// GetFieldTag returns a list of struct field tags
-func GetFieldTag(structure any, tagname string) []string {
-	val := reflect.ValueOf(structure)
-
-	tags := []string{}
-	for i := 0; i < val.Type().NumField(); i++ {
-		tag := val.Type().Field(i).Tag.Get(tagname)
-		tags = append(tags, tag)
-	}
-
-	return tags
-}
-
-// GetFieldJsonTag returns a list of struct field json tags
-func GetFieldJsonTag(structure any) []string {
-	return GetFieldTag(structure, "json")
-}
-
 // GetTypeName get name of type of the variable
 func GetTypeName(myvar any) string {
 	if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
@@ -27,4 +9,9 @@ func GetTypeName(myvar any) string {
 	} else {
 		return t.Name()
 	}
+}
+
+// IsTypeOf checks if the type of first variable is a type of the second variable, returns true if type equals the first variable and false otherwise.
+func IsTypeOf(first, second any) bool {
+	return reflect.TypeOf(first) == reflect.TypeOf(second)
 }
