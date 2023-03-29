a0cffd239f30331dc0a0dfdecc57505e583e4a86
arfanxn <arfan2173@gmail.com>
2023-03-29 16:36:17 +0700

    add Avatar relationship

--- app/models/user.go
+++ app/models/user.go
@@ -14,4 +14,7 @@ type User struct {
 	Password  string       `json:"password"`
 	CreatedAt time.Time    `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt sql.NullTime `json:"updated_at" gorm:"autoUpdateTime"`
+
+	// Avatar relationship
+	Avatar Media `json:"avatar"`
 }
