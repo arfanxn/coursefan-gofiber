d1bb768026ff9ccd2329cb9397edde9dffff0aab
arfanxn <arfan2173@gmail.com>
2023-03-29 22:46:42 +0700

    update register fields tag

--- app/http/requests/auth_register.go
+++ app/http/requests/auth_register.go
@@ -3,9 +3,9 @@ package requests
 import "mime/multipart"
 
 type AuthRegister struct {
-	Name            string `validate:"required,alpha,min=2,max=50"`
-	Email           string `validate:"required,email,max=50"`
-	Password        string `validate:"required,ascii,min=6,max=50"`
-	ConfirmPassword string `validate:"required,eqfield=Password"`
-	Avatar          *multipart.FileHeader
+	Name            string                `json:"name" validate:"required,ascii,min=2,max=50"`
+	Email           string                `json:"email" validate:"required,email,max=50"`
+	Password        string                `json:"password" validate:"required,ascii,min=6,max=50"`
+	ConfirmPassword string                `json:"confirm_password" form:"confirm_password" validate:"required,eqfield=Password"`
+	Avatar          *multipart.FileHeader `json:"avatar" form:"avatar" fhlidate:"required"`
 }
