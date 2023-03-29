106626c7313e56ec07e6c55654a8b60ba0f6a3da
arfanxn <arfan2173@gmail.com>
2023-03-29 15:44:57 +0700

    add GetFileSystemDiskKeys function

--- config/filesystem.go
+++ config/filesystem.go
@@ -12,3 +12,12 @@ var FileSystemDisks = map[string]fileSystemDisk{
 		URL:  "/public",
 	},
 }
+
+// GetFileSystemDiskKeys returns a list of available file system disk keys
+func GetFileSystemDiskKeys() []string {
+	var keys []string
+	for key := range FileSystemDisks {
+		keys = append(keys, key)
+	}
+	return keys
+}
