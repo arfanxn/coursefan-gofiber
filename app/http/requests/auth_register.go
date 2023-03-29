5d31c059f3a6bda276265c774c0757eb0e59d42e
arfanxn <arfan2173@gmail.com>
2023-03-29 17:41:07 +0700

    update req file

--- app/http/requests/auth_register.go
+++ app/http/requests/auth_register.go
@@ -1,8 +1,11 @@
 package requests
 
+import "mime/multipart"
+
 type AuthRegister struct {
 	Name            string `validate:"required,alpha,min=2,max=50"`
 	Email           string `validate:"required,email,max=50"`
 	Password        string `validate:"required,ascii,min=6,max=50"`
 	ConfirmPassword string `validate:"required,eqfield=Password"`
+	Avatar          *multipart.FileHeader
 }
