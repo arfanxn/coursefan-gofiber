7f9195f578ff92d7499733ca50816a04cb317066
arfanxn <arfan2173@gmail.com>
2023-03-30 00:38:38 +0700

    update syncronizer

--- app/helpers/synch/synch.go
+++ app/helpers/synch/synch.go
@@ -24,25 +24,30 @@ func NewSyncronizer() *Syncronizer {
 }
 
 // WG returns pointer of sync.WaitGroup
-func (s Syncronizer) WG() *sync.WaitGroup {
+func (s *Syncronizer) WG() *sync.WaitGroup {
 	return s.waitGroup
 }
 
-// RM returns pointer of sync.RWMutex
-func (s Syncronizer) RM() *sync.RWMutex {
+// RWM returns pointer of sync.RWMutex
+func (s *Syncronizer) RWM() *sync.RWMutex {
 	return s.rwMutex
 }
 
 // M returns pointer of sync.Mutex
-func (s Syncronizer) M() *sync.Mutex {
+func (s *Syncronizer) M() *sync.Mutex {
 	return s.mutex
 }
 
 // Err returns value of errChan, if argument is provided it will set "errChan" with the given argument
-func (s Syncronizer) Err(errs ...error) error {
+func (s *Syncronizer) Err(errs ...error) error {
 	if len(errs) >= 1 {
 		err := errs[0]
 		chanh.ReplaceVal(s.errChan, err)
 	}
 	return chanh.GetValAndKeep(s.errChan)
+}
+
+// Close closes the syncronizer's channel(s)
+func (s *Syncronizer) Close() {
+	close(s.errChan)
 }
