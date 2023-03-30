1410ee7ff4e7d10d884fe11f520d60d483c4a4d5
arfanxn <arfan2173@gmail.com>
2023-03-30 17:55:51 +0700

    make token repo

---
+++ app/repositories/token_repository.go
@@ -0,0 +1,60 @@
+package repositories
+
+import (
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type TokenRepository struct {
+	db *gorm.DB
+}
+
+// NewTokenRepository instantiates a new TokenRepository
+func NewTokenRepository(db *gorm.DB) *TokenRepository {
+	return &TokenRepository{db: db}
+}
+
+// Find finds a token by id
+func (repository *TokenRepository) Find(c *fiber.Ctx, id string) (token models.Token, err error) {
+	err = repository.db.Where("id = ?", id).First(&token).Error
+	return
+}
+
+// FindByTokenableAndType finds a token by tokenable and type
+func (repository *TokenRepository) FindByTokenableAndType(
+	c *fiber.Ctx, tokenableTyp string, tokenableId string, typ string,
+) (token models.Token, err error) {
+	err = repository.db.Where(
+		" `tokenable_type` = ? AND `tokenable_id` = ? AND `type` = ? ",
+		tokenableTyp, tokenableId, typ,
+	).First(&token).Error
+	return
+}
+
+// Insert inserts tokens into the database
+func (repository *TokenRepository) Insert(c *fiber.Ctx, tokens ...*models.Token) (int64, error) {
+	for _, token := range tokens {
+		if token.Id == uuid.Nil {
+			token.Id = uuid.New()
+		}
+		token.CreatedAt = time.Now()
+	}
+	result := repository.db.Create(tokens)
+	return result.RowsAffected, result.Error
+}
+
+// UpdateById
+func (repository *TokenRepository) UpdateById(c *fiber.Ctx, token *models.Token) (int64, error) {
+	result := repository.db.Model(token).Updates(token)
+	return result.RowsAffected, result.Error
+}
+
+// DeleteByIds deletes the entities associated with the given ids
+func (repository *TokenRepository) DeleteByIds(c *fiber.Ctx, tokens ...*models.Token) (int64, error) {
+	result := repository.db.Delete(tokens)
+	return result.RowsAffected, result.Error
+}
