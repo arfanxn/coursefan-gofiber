6ac3ccec2eabf5c857e4e07d4898f9ff343e41b9
arfanxn <arfan2173@gmail.com>
2023-03-30 15:37:52 +0700

    add middleware skipper for some paths

--- app/http/middlewares/auth.go
+++ app/http/middlewares/auth.go
@@ -1,19 +1,23 @@
 package middlewares
 
 import (
+	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"time"
 
 	"github.com/arfanxn/coursefan-gofiber/app/exceptions"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/jwth"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/sliceh"
 	"github.com/arfanxn/coursefan-gofiber/resources"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/skip"
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func Auth() fiber.Handler {
-	return func(c *fiber.Ctx) error {
+	middleware := func(c *fiber.Ctx) error {
 		authCookieName := os.Getenv("AUTH_COOKIE_NAME")
 		token := c.Cookies(authCookieName)
 		if token == "" {
@@ -61,4 +65,23 @@ func Auth() fiber.Handler {
 
 		return c.Next()
 	}
+
+	// Skip authentication at some scenarios
+	return skip.New(middleware, func(c *fiber.Ctx) bool {
+		// Route names to be excluded from authentication middleware
+		excludedPaths := []string{"login", "register", "forgot-password"}
+
+		// Current accessed route path
+		routePath := c.Path()
+
+		// if current route path is contained in excluded paths then it will skip authentication middleware
+		isContain := len(
+			sliceh.Filter(excludedPaths, func(excludedPath string) bool {
+				return regexp.
+					MustCompile(fmt.Sprintf("(%s){1}", excludedPath)).
+					MatchString(routePath)
+			}),
+		) > 0
+		return isContain
+	})
 }
