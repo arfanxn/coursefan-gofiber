10297baa9c00a7627e3aa02f707d3c7d7903e53e
arfanxn <arfan2173@gmail.com>
2023-03-30 13:59:53 +0700

    optimize file header validation with goroutine

--- app/helpers/validationh/validationh.go
+++ app/helpers/validationh/validationh.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 
 	"github.com/arfanxn/coursefan-gofiber/app/exceptions"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/numh"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/sliceh"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/synch"
 	validator_provider "github.com/arfanxn/coursefan-gofiber/app/providers/validators"
 	"github.com/gabriel-vasile/mimetype"
 	ut "github.com/go-playground/universal-translator"
@@ -38,106 +40,131 @@ func ValidateStruct[T any](structure T, lang string) []*exceptions.ValidationErr
 // ValidateStructFileHeader validates the struct fields on *multipart.FileHeader type
 func ValidateStructFileHeader[T any](structure T, lang string) (
 	validationErrs []*exceptions.ValidationError) {
+	syncronizer := synch.NewSyncronizer()
 	structValue := reflect.ValueOf(structure)
 	structType := structValue.Type()
 	for i := 0; i < structValue.NumField(); i++ {
-		field := structType.Field(i)
-		jsonFieldName := strcase.ToSnake(field.Name)
-		jsonTags := strings.Split(field.Tag.Get("json"), ",")
-		if len(jsonTags) >= 1 {
-			jsonFieldName = jsonTags[0]
-		}
-		rules := strings.Split(field.Tag.Get("fhlidate"), ",")
-		fieldValue := structValue.Field(i).Interface()
-		fileHeader, ok := fieldValue.(*multipart.FileHeader)
-		if !ok || (len(rules) == 0) {
-			continue
-		}
-		// Get max upload size from application environment variable
-		maxUploadSize, err := strconv.ParseInt(os.Getenv("MAX_UPLOAD_SIZE"), 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		var (
-			required  bool  = false
-			min       int64 = 0
-			max       int64 = maxUploadSize
-			mimeTypes []string
-		)
-		for _, rule := range rules {
-			switch true {
-			case rule == "required":
-				required = true
-				break
-			case strings.Contains(rule, "min="):
-				num, err := strconv.ParseInt(strings.SplitAfter(rule, "min=")[1], 10, 64)
-				if err != nil {
-					panic(err)
-				}
-				min = num
-				break
-			case strings.Contains(rule, "max="):
-				num, err := strconv.ParseInt(strings.SplitAfter(rule, "max=")[1], 10, 64)
-				if err != nil {
-					panic(err)
-				}
-				max = num
-				break
-			case strings.Contains(rule, "mimes="):
-				mimeTypes = strings.Split(strings.SplitAfter(rule, "mimes=")[1], " ")
-				break
+		syncronizer.WG().Add(1)
+		go func(structField reflect.StructField, fieldValue reflect.Value) {
+			defer syncronizer.WG().Done()
+			if syncronizer.Err() != nil {
+				return
 			}
-		}
-		// if file is not required in rule and the file is not provided then immediately return nil error
-		if !required && (fileHeader == nil) {
-			continue
-		}
-		// check if file is required but not provided
-		if (required) && ((fileHeader == nil) || (fileHeader.Size == 0)) {
-			validationErrs = append(validationErrs,
-				exceptions.NewValidationError(jsonFieldName, jsonFieldName+" is required"))
-			continue
-		}
-		// check if file size is not between the specified min and max size
-		if fileHeader.Size < min || fileHeader.Size > max {
-			validationErrs = append(
-				validationErrs,
-				exceptions.NewValidationError(
-					jsonFieldName,
-					fmt.Sprintf("%s must be between %d and %d size", jsonFieldName, min, max),
-				),
-			)
-			continue
-		}
-		// check if mimetype are available in rules
-		if len(mimeTypes) > 0 {
-			file, err := fileHeader.Open()
+			jsonFieldName := strcase.ToSnake(structField.Name)
+			jsonTags := strings.Split(structField.Tag.Get("json"), ",")
+			if len(jsonTags) >= 1 {
+				jsonFieldName = jsonTags[0]
+			}
+			rules := strings.Split(structField.Tag.Get("fhlidate"), ",")
+			fileHeader, ok := fieldValue.Interface().(*multipart.FileHeader)
+			if !ok || (len(rules) == 0) {
+				return // return if no rules
+			}
+			// Get max upload size from application environment variable
+			maxUploadSize, err := strconv.ParseInt(os.Getenv("MEDIA_MAX_SIZE"), 10, 64)
 			if err != nil {
-				panic(err)
+				syncronizer.Err(err)
+				return
 			}
-			defer file.Close()
-			fileHeaderMime, err := mimetype.DetectReader(file)
-			matchedMimeTypes := sliceh.Filter(mimeTypes, func(mimeType string) bool {
-				return strings.ToLower(fileHeaderMime.String()) == strings.ToLower(mimeType)
-			})
-
-			// if no matching mime types found append an error
-			if len(matchedMimeTypes) == 0 {
+			maxUploadSize = numh.MegabyteToByte(maxUploadSize)
+			var (
+				required  bool  = false
+				min       int64 = 0
+				max       int64 = maxUploadSize
+				mimeTypes []string
+			)
+			for _, rule := range rules {
+				switch true {
+				case rule == "required":
+					required = true
+					break
+				case strings.Contains(rule, "min="):
+					mb, err := strconv.ParseInt(strings.SplitAfter(rule, "min=")[1], 10, 64)
+					if err != nil {
+						syncronizer.Err(err)
+						return
+					}
+					min = numh.MegabyteToByte(mb)
+					break
+				case strings.Contains(rule, "max="):
+					mb, err := strconv.ParseInt(strings.SplitAfter(rule, "max=")[1], 10, 64)
+					if err != nil {
+						syncronizer.Err(err)
+						return
+					}
+					max = numh.MegabyteToByte(mb)
+					break
+				case strings.Contains(rule, "mimes="):
+					mimeTypes = strings.Split(strings.SplitAfter(rule, "mimes=")[1], " ")
+					break
+				}
+			}
+			// if file is not required in rule and the file is not provided then immediately return nil error
+			if !required && (fileHeader == nil) {
+				return
+			}
+			// check if file is required but not provided
+			if (required) && ((fileHeader == nil) || (fileHeader.Size == 0)) {
+				syncronizer.M().Lock()
+				validationErrs = append(validationErrs,
+					exceptions.NewValidationError(jsonFieldName, jsonFieldName+" is required"))
+				syncronizer.M().Unlock()
+				return
+			}
+			// check if file size is not between the specified min and max size
+			if fileHeader.Size < min || fileHeader.Size > max {
+				syncronizer.M().Lock()
 				validationErrs = append(
 					validationErrs,
-					exceptions.NewValidationError(jsonFieldName,
-						fmt.Sprintf(
-							"%s must be a file of types %s",
-							jsonFieldName,
-							strings.Join(mimeTypes, ", "),
-						),
+					exceptions.NewValidationError(
+						jsonFieldName,
+						fmt.Sprintf("%s must be between %d and %d size", jsonFieldName, min, max),
 					),
 				)
-				continue
+				syncronizer.M().Unlock()
+				return
 			}
-		}
+			// check if mimetype are available in rules
+			if len(mimeTypes) > 0 {
+				file, err := fileHeader.Open()
+				if err != nil {
+					syncronizer.Err(err)
+					return
+				}
+				defer file.Close()
+				fileHeaderMime, err := mimetype.DetectReader(file)
+				matchedMimeTypes := sliceh.Filter(mimeTypes, func(mimeType string) bool {
+					return strings.ToLower(fileHeaderMime.String()) == strings.ToLower(mimeType)
+				})
+
+				// if no matching mime types found append an error
+				if len(matchedMimeTypes) == 0 {
+					syncronizer.M().Lock()
+					validationErrs = append(
+						validationErrs,
+						exceptions.NewValidationError(jsonFieldName,
+							fmt.Sprintf(
+								"%s must be a file of types %s",
+								jsonFieldName,
+								strings.Join(mimeTypes, ", "),
+							),
+						),
+					)
+					syncronizer.M().Unlock()
+					return
+				}
+			}
+		}(structType.Field(i), structValue.Field(i))
 	}
-	return
+	syncronizer.WG().Wait()
+
+	if err := syncronizer.Err(); err != nil {
+		panic(err)
+	}
+
+	syncronizer.Close()
+
+	return validationErrs
 }
 
 // TranslateErrs translates errors from validation errors
