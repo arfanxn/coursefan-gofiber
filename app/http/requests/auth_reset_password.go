1dac4746d8a4aab83991cf2a6829531d34785bda
arfanxn <arfan2173@gmail.com>
2023-03-30 21:13:07 +0700

    make AuthResetPassword request model

---
+++ app/http/requests/auth_reset_password.go
@@ -0,0 +1,8 @@
+package requests
+
+type AuthResetPassword struct {
+	Email           string `json:"email" validate:"required,email,max=50"`
+	Otp             string `json:"otp" validate:"required,min=6,max=6"`
+	Password        string `json:"password" validate:"required,ascii,min=6,max=50"`
+	ConfirmPassword string `json:"confirm_password" form:"confirm_password" validate:"required,eqfield=Password"`
+}
