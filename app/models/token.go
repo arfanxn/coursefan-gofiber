c5bf3a67052758f923c1a1b20c125d3afbabce8d
arfanxn <arfan2173@gmail.com>
2023-03-30 15:59:17 +0700

    make token model

---
+++ app/models/token.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"database/sql"
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	TokenBodyNumeric      = []rune("0123456789")
+	TokenBodyAlphaNumeric = []rune("ABCDEFGHIJKLNMOPQRSTUVWXYZ0123456789")
+)
+
+type Token struct {
+	Id            uuid.UUID    `json:"id" gorm:"primaryKey"`
+	TokenableType string       `json:"tokenable_string"`
+	TokenableId   uuid.UUID    `json:"tokenable_id" gorm:"index"`
+	Type          string       `json:"type"`
+	Body          string       `json:"body"` // the token content/body/string
+	UsedAt        sql.NullTime `json:"used_at"`
+	ExpiredAt     sql.NullTime `json:"expired_at"`
+	CreatedAt     time.Time    `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt     sql.NullTime `json:"updated_at" gorm:"autoUpdateTime"`
+
+	// Relations
+
+	Tokenable any `json:"tokenable" gorm:"-"`
+}
+
+// Generate generates a new token and assigns it to models.Token.Body
+func (token *Token) Generate(chars []rune, length int) {
+	for i := 0; i < length; i++ {
+		char := chars[rand.Intn(len(chars))]
+		token.Body = token.Body + string(char)
+	}
+}
