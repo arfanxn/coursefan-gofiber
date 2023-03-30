8a9bded2fda6969ea535e417c82e02ac919ddb97
arfanxn <arfan2173@gmail.com>
2023-03-31 04:23:16 +0700

    update annotation

--- bootstrap/env.go
+++ bootstrap/env.go
@@ -2,7 +2,7 @@ package bootstrap
 
 import "github.com/joho/godotenv"
 
-// UseTestEnv will bootstrap the test environment
+// UseTestEnv will bootstrap environment variables with test environment
 func UseTestEnv() error {
 	testEnvFileName := "test.env"
 	return godotenv.Load(testEnvFileName)
