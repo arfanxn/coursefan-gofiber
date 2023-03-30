255b4aca9815c2eba0519627c590a929e7536c3b
arfanxn <arfan2173@gmail.com>
2023-03-30 13:56:19 +0700

    update annotation

--- app/helpers/numh/numh.go
+++ app/helpers/numh/numh.go
@@ -1,7 +1,7 @@
 // Numh stands for Number Helper
 package numh
 
-// MegabyteToByte converts Megabyte to Byte
+// MegabyteToByte converts Megabyte(s) to Byte(s)
 func MegabyteToByte(mb int64) int64 {
 	return mb * 1024 * 1024
 }
