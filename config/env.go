1cdf4859675097b7af60d1a045be851368d015a5
arfanxn <arfan2173@gmail.com>
2023-03-31 22:13:57 +0700

    update env config

--- config/env.go
+++ config/env.go
@@ -1,10 +1,12 @@
 package config
 
+// Real application
 const (
 	// EnvironmentFileName variable used to specify the name of the environment file
 	EnvironmentFileName = "local.env"
 )
 
+// Test application
 const (
 	// TestEnvironmentFileName variable used to specify the name of the test environment file
 	TestEnvironmentFileName = "test.env"
