42c6d22907f02ecf7fe2b3cf20f264193adb6fc4
arfanxn <arfan2173@gmail.com>
2023-04-01 00:23:42 +0700

    make login test

--- routes/auth_router_test.go
+++ routes/auth_router_test.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/jsonh"
 	"github.com/arfanxn/coursefan-gofiber/app/http/requests"
+	"github.com/arfanxn/coursefan-gofiber/app/providers/databasep"
+	"github.com/arfanxn/coursefan-gofiber/app/repositories"
+	"github.com/arfanxn/coursefan-gofiber/database/factories"
 	"github.com/arfanxn/coursefan-gofiber/resources"
 	"github.com/clarketm/json"
 	"github.com/go-faker/faker/v4"
@@ -17,6 +20,12 @@ import (
 func TestAuthRouter(t *testing.T) {
 	require := require.New(t)
 
+	// Prepare required dependencies
+	c := new(fiber.Ctx)
+	db, err := databasep.GetGormDB()
+	require.Nil(err)
+	userRepository := repositories.NewUserRepository(db)
+
 	t.Run("Register", func(t *testing.T) {
 		var input requests.AuthRegister
 		input.Name = faker.Name()
@@ -24,9 +33,9 @@ func TestAuthRouter(t *testing.T) {
 		input.Password = "111222"
 		input.ConfirmPassword = input.Password
 
-		urlStr := "/api/users/register"
+		urlStr, httpMethod := "/api/users/register", fiber.MethodPost
 		httpRequest := httptest.NewRequest(
-			fiber.MethodPost,
+			httpMethod,
 			urlStr,
 			bytes.NewReader(jsonh.MustMarshal(input)),
 		)
@@ -48,5 +57,38 @@ func TestAuthRouter(t *testing.T) {
 	})
 
 	t.Run("Login", func(t *testing.T) {
+		password := "111222"
+		hashedPassword := "$2a$10$1sGm.uAbtb6h9HkZv1/5S.IFesDq7GOJx0gjXAhGltA3hFssCs/kO"
+		user := factories.User()
+		user.Password = hashedPassword
+		affected, err := userRepository.Insert(c, &user)
+		require.Nil(err)
+		require.NotZero(affected)
+
+		input := requests.AuthLogin{
+			Email:    user.Email,
+			Password: password,
+		}
+		urlStr, httpMethod := "/api/users/login", fiber.MethodPost
+		httpRequest := httptest.NewRequest(
+			httpMethod,
+			urlStr,
+			bytes.NewReader(jsonh.MustMarshal(input)),
+		)
+		httpRequest.Header.Set("Content-Type", "application/json")
+		httpResponse, err := testApp.Test(httpRequest, -1)
+		require.Nil(err)
+		defer httpRequest.Body.Close()
+
+		expectedResponse := resources.Response{
+			Code:    fiber.StatusOK,
+			Message: "Login successfully",
+		}
+		var actualResponse resources.Response
+		err = json.NewDecoder(httpResponse.Body).Decode(&actualResponse)
+		require.Nil(err)
+
+		require.Equal(expectedResponse.Code, httpResponse.StatusCode)
+		require.Equal(expectedResponse.Code, actualResponse.Code)
 	})
 }
