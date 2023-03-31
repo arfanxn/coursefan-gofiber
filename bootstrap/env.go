3dde15e5e0c6399d61cb907336a4e590d230f4b3
arfanxn <arfan2173@gmail.com>
2023-03-31 22:14:46 +0700

    delete env file on bootstrap

--- bootstrap/env.go
+++
@@ -1,9 +0,0 @@
-package bootstrap
-
-import "github.com/joho/godotenv"
-
-// ConfigureTestENV will bootstrap environment variables with test environment into application
-func ConfigureTestENV() error {
-	testEnvFileName := "test.env"
-	return godotenv.Load(testEnvFileName)
-}
