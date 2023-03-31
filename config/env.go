614f74099aa85f666211379a4c0ee713253626dc
arfanxn <arfan2173@gmail.com>
2023-03-31 22:13:46 +0700

    update env config

--- config/env.go
+++ config/env.go
@@ -1,4 +1,11 @@
 package config
 
-// EnvironmentFileName variable used to specify the name of the environment file
-const EnvironmentFileName = "local.env"
+const (
+	// EnvironmentFileName variable used to specify the name of the environment file
+	EnvironmentFileName = "local.env"
+)
+
+const (
+	// TestEnvironmentFileName variable used to specify the name of the test environment file
+	TestEnvironmentFileName = "test.env"
+)
