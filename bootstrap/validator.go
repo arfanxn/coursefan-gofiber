187b425980f0c46de45f499fa1bc9d43af6194cc
arfanxn <arfan2173@gmail.com>
2023-03-31 22:38:57 +0700

    update bootstrap

---
+++ bootstrap/validator.go
@@ -0,0 +1,23 @@
+package bootstrap
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// Validator bootstraps application validator
+func Validator() error {
+	validate := validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		// skip if tag key says it should be ignored
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	return nil
+}
