670384906837302892130bf06c031e359a3f36a1
arfanxn <arfan2173@gmail.com>
2023-04-01 22:58:24 +0700

    update wallet model

--- app/models/wallet.go
+++ app/models/wallet.go
@@ -9,8 +9,8 @@ import (
 
 type Wallet struct {
 	Id        uuid.UUID `json:"id" gorm:"primaryKey;type:char(36)"`
-	UserId    uuid.UUID `json:"user_id" gorm:"type:CHAR(36);NOT NULL"`
-	User      User      `json:"user" gorm:"foreignKey:UserId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	OwnerId   uuid.UUID `json:"user_id" gorm:"type:CHAR(36);NOT NULL"`
+	Owner     User      `json:"user" gorm:"foreignKey:OwnerId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Balance   int64     `json:"balance"  gorm:"type:BIGINT UNSIGNED NOT NULL;default:0"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
 	UpdatedAt null.Time `json:"updated_at" gorm:"autoUpdateTime"`
