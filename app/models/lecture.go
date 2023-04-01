92a059824c60191e568a10d6a49f90d36b7e571f
arfanxn <arfan2173@gmail.com>
2023-04-01 23:40:49 +0700

    make lecture and part models

---
+++ app/models/lecture.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
+)
+
+type Lecture struct {
+	Id        uuid.UUID `json:"id" gorm:"primaryKey;type:char(36)"`
+	CourseId  uuid.UUID `json:"course_id" gorm:"type:CHAR(36);NOT NULL"`
+	Course    Course    `json:"course" gorm:"foreignKey:CourseId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Name      string    `json:"name" gorm:"type:VARCHAR(50);NOT NULL"`
+	Order     int       `json:"order" gorm:"type:INTEGER UNSIGNED NOT NULL;default:0"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
+	UpdatedAt null.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
