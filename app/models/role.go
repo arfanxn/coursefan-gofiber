e461b76c6c2934ece3883e11a27d18a2bea2958e
arfanxn <arfan2173@gmail.com>
2023-04-01 16:11:49 +0700

    fix error on permission and role models

--- app/models/role.go
+++ app/models/role.go
@@ -3,6 +3,6 @@ package models
 import "github.com/google/uuid"
 
 type Role struct {
-	Id   uuid.UUID `json:"id" gorm:"primaryKey"`
+	Id   uuid.UUID `json:"id" gorm:"primaryKey;type:CHAR(36)"`
 	Name string    `json:"name" gorm:"index;type:VARCHAR(25) NOT NULL"`
 }
