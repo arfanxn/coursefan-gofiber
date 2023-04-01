d9502e1a40c47e4633760758abdad272af93ee39
arfanxn <arfan2173@gmail.com>
2023-04-01 16:01:24 +0700

    update user model and resource

--- app/models/user.go
+++ app/models/user.go
@@ -8,12 +8,13 @@ import (
 )
 
 type User struct {
-	Id        uuid.UUID    `json:"id" gorm:"primaryKey;type:CHAR(36)"`
-	Name      string       `json:"name" gorm:"index;type:VARCHAR(50) NOT NULL"`
-	Email     string       `json:"email" gorm:"uniqueIndex;type:VARCHAR(50) NOT NULL"`
-	Password  string       `json:"password" gorm:"type:VARCHAR(256)"`
-	CreatedAt time.Time    `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
-	UpdatedAt sql.NullTime `json:"updated_at" gorm:"autoUpdateTime"`
+	Id        uuid.UUID      `json:"id" gorm:"primaryKey;type:CHAR(36)"`
+	Name      string         `json:"name" gorm:"index;type:VARCHAR(50) NOT NULL"`
+	Email     string         `json:"email" gorm:"uniqueIndex;type:VARCHAR(50) NOT NULL"`
+	Password  string         `json:"password" gorm:"type:VARCHAR(256) NOT NULL"`
+	Biography sql.NullString `json:"biography" gorm:"type:VARCHAR(256)"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
+	UpdatedAt sql.NullTime   `json:"updated_at" gorm:"autoUpdateTime"`
 
 	// Avatar relationship
 	Avatar Media `json:"avatar" gorm:"-"`
