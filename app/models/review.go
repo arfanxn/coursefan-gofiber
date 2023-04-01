5d49bc4647b05e86d0eef0ad6e1c7769da9c295d
arfanxn <arfan2173@gmail.com>
2023-04-01 23:22:28 +0700

    update json tag

--- app/models/review.go
+++ app/models/review.go
@@ -9,8 +9,8 @@ import (
 
 type Review struct {
 	Id             uuid.UUID `json:"id" gorm:"primaryKey;type:char(36)"`
-	ReviewableType string    `json:"model_type" gorm:"index;type:VARCHAR(25) NOT NULL"`
-	ReviewableId   uuid.UUID `json:"model_id" gorm:"type:VARCHAR(36) NOT NULL"`
+	ReviewableType string    `json:"reviewable_type" gorm:"index;type:VARCHAR(25) NOT NULL"`
+	ReviewableId   uuid.UUID `json:"reviewable_id" gorm:"type:VARCHAR(36) NOT NULL"`
 	ReviewerId     uuid.UUID `json:"reviewer_id" gorm:"type:CHAR(36);NOT NULL"`
 	Reviewer       User      `json:"reviewer" gorm:"foreignKey:ReviewerId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Rate           int       `json:"collection_name"  gorm:"type:TINYINT UNSIGNED NOT NULL"`
