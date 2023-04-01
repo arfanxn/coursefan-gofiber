bfbb59780fc38498c4d051e8565c0823ecc582bd
arfanxn <arfan2173@gmail.com>
2023-04-01 22:55:31 +0700

    add timestamps on pivot models

--- app/models/permission_role.go
+++ app/models/permission_role.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
 )
 
 type PermissionRole struct {
@@ -9,6 +12,8 @@ type PermissionRole struct {
 	Permission   Permission `json:"permission" gorm:"foreignKey:PermissionId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	RoleId       uuid.UUID  `json:"role_id" gorm:"type:CHAR(36);NOT NULL"`
 	Role         Role       `json:"role" gorm:"foreignKey:RoleId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt    time.Time  `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
+	UpdatedAt    null.Time  `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 func (PermissionRole) TableName() string {
--- app/models/transaction_wallet.go
+++ app/models/transaction_wallet.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
 )
 
 type TransactionWallet struct {
@@ -11,6 +14,8 @@ type TransactionWallet struct {
 	Sender        Wallet      `json:"sender" gorm:"foreignKey:SenderId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	ReceiverId    uuid.UUID   `json:"receiver_id" gorm:"type:CHAR(36);NOT NULL"`
 	Receiver      Wallet      `json:"receiver" gorm:"foreignKey:ReceiverId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt     time.Time   `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
+	UpdatedAt     null.Time   `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 func (TransactionWallet) TableName() string {
