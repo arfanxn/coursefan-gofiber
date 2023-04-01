752730ad43901909a14af4d87063f3e116496878
arfanxn <arfan2173@gmail.com>
2023-04-02 00:52:44 +0700

    make user setting enum and factory

---
+++ app/enums/user_setting.go
@@ -0,0 +1,11 @@
+package enums
+
+// models.UserSetting.Key enums
+const (
+	// UserSettingKeyUserPublicProfile represents whether the user's profile is public (can be viewed) or not
+	UserSettingKeyUserPublicProfile string = "user_public_profile"
+	// UserSettingKeyUserPublicCourse represents whether the user's course(s) is public (can be viewed) or not
+	UserSettingKeyUserPublicCourse string = "user_public_course"
+	// UserSettingKeyNotificationAcceptPromotion represents whether the user notification is acceptiong promotion(s)
+	UserSettingKeyNotificationAcceptPromotion string = "notification_accept_promotion"
+)
---
+++ database/factories/user_setting.go
@@ -0,0 +1,27 @@
+package factories
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/enums"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/sliceh"
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	"gopkg.in/guregu/null.v4"
+)
+
+// FakeUserSetting returns fake data of models.UserSetting
+func FakeUserSetting() models.UserSetting {
+	return models.UserSetting{
+		// Id: will be filled in later
+		// UserId: will be filled in later
+		// User: will be filled in later
+		Key:       enums.UserSettingKeyUserPublicProfile,
+		Value:     strconv.FormatBool(sliceh.Random(true, false)),
+		CreatedAt: time.Now(),
+		UpdatedAt: sliceh.Random(
+			null.NewTime(time.Now(), true),
+			null.NewTime(time.Time{}, false),
+		),
+	}
+}
