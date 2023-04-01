a3e74d1f20875d9703634481f96745669aaa3c61
arfanxn <arfan2173@gmail.com>
2023-04-02 02:14:06 +0700

    make lecture factory

---
+++ database/factories/lecture.go
@@ -0,0 +1,26 @@
+package factories
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/sliceh"
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	"github.com/go-faker/faker/v4"
+	"gopkg.in/guregu/null.v4"
+)
+
+func FakeLecture() models.Lecture {
+	return models.Lecture{
+		// Id:, // will be filled in later
+		// CourseId:, // will be filled in later
+		// Course:, // will be filled in later
+		Name:      faker.Word(),
+		Order:     rand.Intn(10),
+		CreatedAt: time.Now(),
+		UpdatedAt: sliceh.Random(
+			null.NewTime(time.Now(), true),
+			null.NewTime(time.Time{}, false),
+		),
+	}
+}
