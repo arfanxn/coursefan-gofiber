27ad2592724ece278887140a8752377f74ecb740
arfanxn <arfan2173@gmail.com>
2023-04-01 16:39:08 +0700

    remove column indexing

--- app/models/review.go
+++ app/models/review.go
@@ -13,7 +13,7 @@ type Review struct {
 	ReviewableId   uuid.UUID    `json:"model_id" gorm:"type:VARCHAR(36) NOT NULL"`
 	ReviewerId     uuid.UUID    `json:"reviewer_id" gorm:"type:CHAR(36);NOT NULL"`
 	Reviewer       User         `json:"reviewer" gorm:"foreignKey:ReviewerId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Rate           int          `json:"collection_name"  gorm:"index;type:TINYINT UNSIGNED NOT NULL"`
+	Rate           int          `json:"collection_name"  gorm:"type:TINYINT UNSIGNED NOT NULL"`
 	Title          string       `json:"title" gorm:"type:VARCHAR(50) NOT NULL"`
 	Body           string       `json:"body" gorm:"type:VARCHAR(256) NOT NULL"`
 	CreatedAt      time.Time    `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
