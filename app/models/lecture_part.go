40074281d86d5862c72deff6051eac0ac610045b
arfanxn <arfan2173@gmail.com>
2023-04-03 00:02:30 +0700

    remove decription field

--- app/models/lecture_part.go
+++ app/models/lecture_part.go
@@ -8,12 +8,11 @@ import (
 )
 
 type LecturePart struct {
-	Id          uuid.UUID   `json:"id" gorm:"primaryKey;type:char(36)"`
-	CourseId    uuid.UUID   `json:"course_id" gorm:"type:CHAR(36);NOT NULL"`
-	Course      Course      `json:"course" gorm:"foreignKey:CourseId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Part        int         `json:"part"  gorm:"type:INTEGER UNSIGNED NOT NULL"`
-	Name        string      `json:"title" gorm:"type:VARCHAR(50) NOT NULL"`
-	Description null.String `json:"description" gorm:"type:VARCHAR(256)"`
-	CreatedAt   time.Time   `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
-	UpdatedAt   null.Time   `json:"updated_at" gorm:"autoUpdateTime"`
+	Id        uuid.UUID `json:"id" gorm:"primaryKey;type:char(36)"`
+	CourseId  uuid.UUID `json:"course_id" gorm:"type:CHAR(36);NOT NULL"`
+	Course    Course    `json:"course" gorm:"foreignKey:CourseId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Part      int       `json:"part"  gorm:"type:INTEGER UNSIGNED NOT NULL"`
+	Name      string    `json:"title" gorm:"type:VARCHAR(50) NOT NULL"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
+	UpdatedAt null.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
