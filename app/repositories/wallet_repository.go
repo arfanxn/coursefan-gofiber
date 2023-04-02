82eab0cbca5e5e988d18bb37584dff520493d267
arfanxn <arfan2173@gmail.com>
2023-04-02 15:49:41 +0700

    make wallet repository

---
+++ app/repositories/wallet_repository.go
@@ -0,0 +1,59 @@
+package repositories
+
+import (
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
+	"gorm.io/gorm"
+)
+
+type WalletRepository struct {
+	db *gorm.DB
+}
+
+// NewWalletRepository instantiates a new WalletRepository
+func NewWalletRepository(db *gorm.DB) *WalletRepository {
+	return &WalletRepository{db: db}
+}
+
+// All returns all wallets in the database
+func (repository *WalletRepository) All(c *fiber.Ctx) (wallets []models.Wallet, err error) {
+	err = repository.db.Find(&wallets).Error
+	return
+}
+
+// Find finds model by id
+func (repository *WalletRepository) Find(c *fiber.Ctx, id string) (wallet models.Wallet, err error) {
+	err = repository.db.Where("id = ?", id).First(&wallet).Error
+	return
+}
+
+// Insert inserts models into the database
+func (repository *WalletRepository) Insert(c *fiber.Ctx, wallets ...*models.Wallet) (int64, error) {
+	for _, wallet := range wallets {
+		if wallet.Id == uuid.Nil {
+			wallet.Id = uuid.New()
+		}
+		wallet.CreatedAt = time.Now()
+	}
+	result := repository.db.Create(wallets)
+	return result.RowsAffected, result.Error
+}
+
+// UpdateById updates model in the database by given id
+func (repository *WalletRepository) UpdateById(c *fiber.Ctx, wallet *models.Wallet) (int64, error) {
+	// refresh model updated at
+	wallet.UpdatedAt = null.NewTime(time.Now(), true)
+	// update
+	result := repository.db.Model(wallet).Updates(wallet)
+	return result.RowsAffected, result.Error
+}
+
+// DeleteByIds deletes the entities associated with the given ids
+func (repository *WalletRepository) DeleteByIds(c *fiber.Ctx, wallets ...*models.Wallet) (int64, error) {
+	result := repository.db.Delete(wallets)
+	return result.RowsAffected, result.Error
+}
