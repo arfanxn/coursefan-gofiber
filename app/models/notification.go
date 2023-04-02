f6d8cac7136dc9b5919005519e2e65860a6cd347
arfanxn <arfan2173@gmail.com>
2023-04-02 15:36:24 +0700

    update notification model

--- app/models/notification.go
+++ app/models/notification.go
@@ -5,20 +5,20 @@ import (
 
 	"github.com/google/uuid"
 	"gopkg.in/guregu/null.v4"
-)
+)``
 
 type Notification struct {
-	Id         uuid.UUID   `json:"id" gorm:"primaryKey;type:char(36)"`
-	SenderId   uuid.UUID   `json:"sender_id" gorm:"type:CHAR(36);NOT NULL"`
-	Sender     User        `json:"sender" gorm:"foreignKey:SenderId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	ReceiverId uuid.UUID   `json:"receiver_id" gorm:"type:CHAR(36);NOT NULL"`
-	Receiver   User        `json:"receiver" gorm:"foreignKey:ReceiverId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	ObjectType string      `json:"object_type" gorm:"index;type:VARCHAR(25) NOT NULL"`
-	ObjectId   uuid.UUID   `json:"object_id" gorm:"type:VARCHAR(36) NOT NULL"`
-	Title      string      `json:"title" gorm:"type:VARCHAR(50) NOT NULL"`
-	Body       null.String `json:"body" gorm:"type:TEXT"`
-	Type       string      `json:"type"  gorm:"type:VARCHAR(25) NOT NULL"`
-	ReadedAt   null.Time   `json:"readed_at" gorm:"type:DATETIME(3)"`
-	CreatedAt  time.Time   `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
-	UpdatedAt  null.Time   `json:"updated_at" gorm:"autoUpdateTime"`
+	Id         uuid.UUID     `json:"id" gorm:"primaryKey;type:char(36)"`
+	SenderId   uuid.UUID     `json:"sender_id" gorm:"type:CHAR(36);NOT NULL"`
+	Sender     User          `json:"sender" gorm:"foreignKey:SenderId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ReceiverId uuid.UUID     `json:"receiver_id" gorm:"type:CHAR(36);NOT NULL"`
+	Receiver   User          `json:"receiver" gorm:"foreignKey:ReceiverId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ObjectType null.String   `json:"object_type" gorm:"index;type:VARCHAR(25)"`
+	ObjectId   uuid.NullUUID `json:"object_id" gorm:"type:VARCHAR(36)"`
+	Title      string        `json:"title" gorm:"type:VARCHAR(50) NOT NULL"`
+	Body       null.String   `json:"body" gorm:"type:TEXT"`
+	Type       string        `json:"type"  gorm:"type:VARCHAR(25) NOT NULL"`
+	ReadedAt   null.Time     `json:"readed_at" gorm:"type:DATETIME(3)"`
+	CreatedAt  time.Time     `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
+	UpdatedAt  null.Time     `json:"updated_at" gorm:"autoUpdateTime"`
 }
