553d4f5543c79f09e02110727dcc24355e45e3b7
arfanxn <arfan2173@gmail.com>
2023-04-04 17:31:41 +0700

    update helper func name

--- app/helpers/rwh/rwh.go
+++ app/helpers/rwh/rwh.go
@@ -7,7 +7,7 @@ import (
 	"github.com/clarketm/json"
 )
 
-func StructToReadCloser(v any) (readerCloser io.ReadCloser, err error) {
+func AnyToReadCloser(v any) (readerCloser io.ReadCloser, err error) {
 	marshalled, err := json.Marshal(v)
 	if err != nil {
 		return
