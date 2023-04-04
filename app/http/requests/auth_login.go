ab724d0eab4753260ade4b83ed330bca126e1f45
arfanxn <arfan2173@gmail.com>
2023-04-04 15:42:17 +0700

    update auth login request

--- app/http/requests/auth_login.go
+++ app/http/requests/auth_login.go
@@ -1,6 +1,6 @@
 package requests
 
 type AuthLogin struct {
-	Email    string `validate:"required,email,max=50"`
-	Password string `validate:"required,ascii,min=6,max=50"`
+	Email    string `json:"email" validate:"required,email,max=50"`
+	Password string `json:"password" validate:"required,ascii,min=6,max=50"`
 }
