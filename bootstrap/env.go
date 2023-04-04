1dbd8693eb3e8712c4413224642d2f6618ef0add
arfanxn <arfan2173@gmail.com>
2023-04-04 22:27:08 +0700

    update env bootstrap

--- bootstrap/env.go
+++ bootstrap/env.go
@@ -1,12 +1,18 @@
 package bootstrap
 
 import (
+	"os"
+
 	"github.com/arfanxn/coursefan-gofiber/config"
 	"github.com/joho/godotenv"
 )
 
 // ENV will bootstraping application environment variables
 func ENV() (err error) {
+	// Check if environment is loaded before bootstrap, if already loaded then immediately return
+	if os.Getenv("APP_NAME") != "" {
+		return
+	}
 	err = godotenv.Load(config.EnvironmentFileName)
 	return
 }
