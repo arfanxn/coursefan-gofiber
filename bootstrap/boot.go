4cfc775e2d9593381dcf84bfefaa7a1a97a183f4
arfanxn <arfan2173@gmail.com>
2023-04-04 22:29:56 +0700

    make boot

---
+++ bootstrap/boot.go
@@ -0,0 +1,16 @@
+package bootstrap
+
+// Boot will bootstraps everything that needs to be bootstrapped
+func Boot() error {
+	errs := []error{
+		ENV(),
+		Logger(),
+		FileSystem(),
+	}
+	for _, err := range errs {
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
