79f8253baaff7bed877c8f565d2efbd994ad8fcd
arfanxn <arfan2173@gmail.com>
2023-04-04 22:24:19 +0700

    update file system config

--- config/file_system.go
+++ config/file_system.go
@@ -1,12 +1,9 @@
 package config
 
-type fileSystemDisk struct {
-	Root string // the root of the filesystem
-	URL  string // the URL to access file
-}
+import storage "github.com/ramadani/go-filestorage"
 
 // FileSystemDisks (FileSystemDisks) represents a disk configuration
-var FileSystemDisks = map[string]fileSystemDisk{
+var FileSystemDisks = map[string]storage.Config{
 	"public": {
 		Root: "./public",
 		URL:  "/public",
