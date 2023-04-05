deaf46dc532f24dbd1b36b31d72cdd0574350232
arfanxn <arfan2173@gmail.com>
2023-04-05 23:15:14 +0700

    make course router

---
+++ routes/course_router.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/arfanxn/coursefan-gofiber/app/providers/controllerp"
+	"github.com/arfanxn/coursefan-gofiber/app/providers/databasep"
+	"github.com/gofiber/fiber/v2"
+)
+
+// registerCourseRouter registers course module routes into the router
+func registerCourseRouter(router fiber.Router) {
+	courseController := controllerp.InitCourseController(databasep.MustGetGormDB())
+	courses := router.Group("/courses")
+	courses.Get("", courseController.All)
+}
