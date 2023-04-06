081d6f600f410927d7bece59232aaa8430990e17
arfanxn <arfan2173@gmail.com>
2023-04-07 06:04:06 +0700

    update model

--- app/models/lecture_part.go
+++ app/models/lecture_part.go
@@ -10,7 +10,7 @@ import (
 type LecturePart struct {
 	Id        uuid.UUID `json:"id" gorm:"primaryKey;type:char(36)"`
 	CourseId  uuid.UUID `json:"course_id" gorm:"type:CHAR(36);NOT NULL"`
-	Course    Course    `json:"course" gorm:"foreignKey:CourseId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Course    *Course   `json:"course" gorm:"foreignKey:CourseId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Part      int       `json:"part"  gorm:"type:INTEGER UNSIGNED NOT NULL"`
 	Name      string    `json:"title" gorm:"type:VARCHAR(50) NOT NULL"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
