dab5750eb9dd83fe1d6b12137b759d161a582a30
arfanxn <arfan2173@gmail.com>
2023-04-07 04:57:27 +0700

    add IsGormError function

--- app/helpers/errorh/errorh.go
+++ app/helpers/errorh/errorh.go
@@ -6,10 +6,43 @@ import (
 	"gorm.io/gorm"
 )
 
-// IsGormErrRecordNotFound cheks whether the given err is an error of gorm.ErrRecordNotFound, return true if it is
+// IsGormErrRecordNotFound checks whether the given err is an error of gorm.ErrRecordNotFound, return true if it is
 func IsGormErrRecordNotFound(errAny any) bool {
 	if err, ok := errAny.(error); ok {
 		return errors.Is(err, gorm.ErrRecordNotFound)
+	}
+	return false
+}
+
+// IsGormError checks whether the given err is an errors from gorm package, return true if it is
+func IsGormError(errAny any) bool {
+	gormErrs := []error{
+		gorm.ErrRecordNotFound,
+		gorm.ErrInvalidTransaction,
+		gorm.ErrNotImplemented,
+		gorm.ErrMissingWhereClause,
+		gorm.ErrUnsupportedRelation,
+		gorm.ErrPrimaryKeyRequired,
+		gorm.ErrModelValueRequired,
+		gorm.ErrModelAccessibleFieldsRequired,
+		gorm.ErrSubQueryRequired,
+		gorm.ErrInvalidData,
+		gorm.ErrUnsupportedDriver,
+		gorm.ErrRegistered,
+		gorm.ErrInvalidField,
+		gorm.ErrEmptySlice,
+		gorm.ErrDryRunModeUnsupported,
+		gorm.ErrInvalidDB,
+		gorm.ErrInvalidValue,
+		gorm.ErrInvalidValueOfLength,
+		gorm.ErrPreloadNotAllowed,
+	}
+	if err, ok := errAny.(error); ok {
+		for _, gormErr := range gormErrs {
+			if errors.Is(err, gormErr) {
+				return true
+			}
+		}
 	}
 	return false
 }
