1a31418019077cf8457e607bf7ecd5a3e806e591
arfanxn <arfan2173@gmail.com>
2023-04-06 22:37:08 +0700

    add validator exception

--- app/exceptions/validator_exception.go
+++ app/exceptions/validator_exception.go
@@ -2,12 +2,29 @@ package exceptions
 
 import "github.com/gofiber/fiber/v2"
 
+type ValidationErrors struct {
+	Errors []*ValidationError
+}
+
+func NewValidationErrors(errs []*ValidationError) *ValidationErrors {
+	validationErrs := &ValidationErrors{}
+	validationErrs.Errors = errs
+	return validationErrs
+}
+
+func (validationErrs *ValidationErrors) Error() string {
+	if len(validationErrs.Errors) == 0 {
+		return ""
+	}
+	return validationErrs.Errors[0].Error()
+}
+
 type ValidationError struct {
 	Field, Message string
 }
 
-func (ValidationError *ValidationError) Error() string {
-	return ValidationError.Message
+func (validationErr *ValidationError) Error() string {
+	return validationErr.Message
 }
 
 func NewValidationError(field, message string) *ValidationError {
