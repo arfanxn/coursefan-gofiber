b26147f43744c0baf2b90988b81177f7ba5a28af
arfanxn <arfan2173@gmail.com>
2023-04-08 00:29:59 +0700

    fix typo

--- routes/lecture_part_router.go
+++ routes/lecture_part_router.go
@@ -9,6 +9,6 @@ import (
 // registerLecturePartRouter registers lecture part module routes into the router
 func registerLecturePartRouter(router fiber.Router) {
 	lecturePartController := controllerp.InitLecturePartController(databasep.MustGetGormDB())
-	coursesIdLectureParts := router.Group("/courses/{course_id}/lecture_parts")
+	coursesIdLectureParts := router.Group("/courses/:course_id/lecture_parts")
 	coursesIdLectureParts.Get("", lecturePartController.AllByCourse)
 }
