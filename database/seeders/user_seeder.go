c2f10ac68bfa06dab0287bb099674c9577abae10
arfanxn <arfan2173@gmail.com>
2023-04-07 21:11:17 +0700

    update user seder

--- database/seeders/user_seeder.go
+++ database/seeders/user_seeder.go
@@ -1,10 +1,16 @@
 package seeders
 
 import (
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/errorh"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/sliceh"
 	"github.com/arfanxn/coursefan-gofiber/app/models"
 	"github.com/arfanxn/coursefan-gofiber/app/repositories"
 	"github.com/arfanxn/coursefan-gofiber/database/factories"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/guregu/null.v4"
 )
 
 type UserSeeder struct {
@@ -21,7 +27,19 @@ func NewUserSeeder(repository *repositories.UserRepository) *UserSeeder {
 // Run runs the seeder
 func (seeder *UserSeeder) Run(c *fiber.Ctx) (err error) {
 	var users []*models.User
-	for i := 0; i < 50; i++ {
+
+	users = append(users, &models.User{
+		Name:      "Muhammad Arfan",
+		Email:     "arf@gm.com",
+		Password:  string(errorh.Must(bcrypt.GenerateFromPassword([]byte("111222"), bcrypt.DefaultCost))),
+		CreatedAt: time.Now(),
+		UpdatedAt: sliceh.Random(
+			null.NewTime(time.Now(), true),
+			null.NewTime(time.Time{}, false),
+		),
+	})
+
+	for i := 0; i < 49; i++ {
 		user := factories.FakeUser()
 		users = append(users, &user)
 	}
