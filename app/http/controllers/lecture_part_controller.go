f57b5694210863842d1132b97e3c655caa9413a1
arfanxn <arfan2173@gmail.com>
2023-04-07 23:50:15 +0700

    update lecture  part controller

--- app/http/controllers/lecture_part_controller.go
+++ app/http/controllers/lecture_part_controller.go
@@ -3,22 +3,34 @@ package controllers
 import (
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/responseh"
 	"github.com/arfanxn/coursefan-gofiber/app/http/requests"
+	"github.com/arfanxn/coursefan-gofiber/app/policies"
 	"github.com/arfanxn/coursefan-gofiber/app/services"
 	"github.com/arfanxn/coursefan-gofiber/resources"
 	"github.com/gofiber/fiber/v2"
 )
 
 type LecturePartController struct {
+	policy  *policies.LecturePartPolicy
 	service *services.LecturePartService
 }
 
-func (controller *LecturePartController) GetByCourse(c *fiber.Ctx) (err error) {
+func NewLecturePartController(
+	policy *policies.LecturePartPolicy,
+	service *services.LecturePartService,
+) *LecturePartController {
+	return &LecturePartController{
+		policy:  policy,
+		service: service,
+	}
+}
+
+func (controller *LecturePartController) AllByCourse(c *fiber.Ctx) (err error) {
 	input := requests.Query{}
 	err = input.FromContext(c)
 	if err != nil {
 		return
 	}
-	pagination, err := controller.service.GetByCourse(c, input)
+	pagination, err := controller.service.AllByCourse(c, input)
 	if err != nil {
 		return err
 	}
