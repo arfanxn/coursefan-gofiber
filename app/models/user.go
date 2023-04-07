0e577b88717744eab0f0c3dc544f3fa6657bd2c5
arfanxn <arfan2173@gmail.com>
2023-04-07 20:18:43 +0700

    upddate user's  field

--- app/models/user.go
+++ app/models/user.go
@@ -16,5 +16,5 @@ type User struct {
 	UpdatedAt null.Time `json:"updated_at" gorm:"autoUpdateTime"`
 
 	// Avatar relationship
-	Avatar Media `json:"avatar" gorm:"-"`
+	Avatar *Media `json:"avatar" gorm:"-"`
 }
