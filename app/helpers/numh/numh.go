bfcc093007f726b128a9a3c5ac6e05a5e9190618
arfanxn <arfan2173@gmail.com>
2023-04-07 14:58:37 +0700

    add random number generator

--- app/helpers/numh/numh.go
+++ app/helpers/numh/numh.go
@@ -1,7 +1,18 @@
 // Numh stands for Number Helper
 package numh
 
+import (
+	"math/rand"
+	"time"
+)
+
 // MegabyteToByte converts Megabyte(s) to Byte(s)
 func MegabyteToByte(mb int64) int64 {
 	return mb * 1024 * 1024
+}
+
+// Random generates random numbers on range
+func Random(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
 }
