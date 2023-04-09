b9ce04816027a47ea6ad43835920a06d2ee74d63
arfanxn <arfan2173@gmail.com>
2023-04-09 23:18:07 +0700

    update how dynamic query filter works

--- app/helpers/gormh/gormh.go
+++ app/helpers/gormh/gormh.go
@@ -3,133 +3,175 @@ package gormh
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-	"time"
 
+	"github.com/arfanxn/coursefan-gofiber/app/enums"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/sliceh"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/strh"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/synch"
 	"github.com/arfanxn/coursefan-gofiber/app/http/requests"
+	"github.com/arfanxn/coursefan-gofiber/app/models"
 	"gorm.io/gorm"
 )
 
 // BuildFromRequestQuery builds a gorm query from the given requests.Query
 func BuildFromRequestQuery(tx *gorm.DB, query requests.Query) *gorm.DB {
+	mainModel := models.User{}
+	tx = tx.Table(mainModel.TableName())
 	syncronizer := synch.NewSyncronizer()
 	defer syncronizer.Close()
-	scopes := [](func(*gorm.DB) *gorm.DB){}
 	for _, filter := range query.Filters {
-		syncronizer.WG().Add(1)
-		go func(filter requests.QueryFilter) {
-			defer syncronizer.WG().Done()
-			syncronizer.M().Lock()
-			scopes = append(scopes, buildWhereScopeFromRequestQueryFilter(tx, filter))
-			syncronizer.M().Unlock()
-		}(filter)
-	}
-	for column, orderingType := range query.OrderBys {
-		syncronizer.WG().Add(1)
-		go func(column, orderingType string) {
-			defer syncronizer.WG().Done()
-			syncronizer.M().Lock()
-			tx = tx.Order(column + " " + strings.ToLower(orderingType))
-			syncronizer.M().Unlock()
-		}(column, orderingType)
-	}
-	for _, with := range query.Withs {
-		if with == "" {
+		if filter.TableName() != mainModel.TableName() {
 			continue
 		}
-		syncronizer.WG().Add(1)
-		go func(with string) {
-			defer syncronizer.WG().Done()
-			syncronizer.M().Lock()
-			tx = tx.Preload(with, func(tx *gorm.DB) *gorm.DB { return tx.Order("created_at asc") })
-			syncronizer.M().Unlock()
-		}(with)
+		filter.CastValues()
+		tx = buildFromRequestQueryFilter(tx, filter)
 	}
-	syncronizer.WG().Wait()
-	if len(scopes) != 0 {
-		tx = tx.Scopes(scopes...)
+	for _, include := range query.Includes {
+		tx.Preload(strh.StrToDelimetedCamel(include.Relation, "."), func(tx *gorm.DB) *gorm.DB {
+			relationTableNames := include.TableNames()
+			lastRelationTableName := sliceh.Last(relationTableNames)
+			for _, filter := range query.Filters {
+				if filter.TableName() != lastRelationTableName {
+					continue
+				}
+				tx = buildFromRequestQueryFilter(tx, filter)
+			}
+			for _, sort := range query.Sorts {
+				if sort.TableName() != lastRelationTableName {
+					continue
+				}
+				tx = tx.Order(fmt.Sprintf("%s %s", sort.Column, sort.Direction))
+			}
+			if include.Limit.Valid {
+				tx = tx.Limit(int(include.Limit.Int64))
+			} else if query.Limit.Valid {
+				tx = tx.Limit(int(query.Limit.Int64))
+			}
+			return tx
+		})
 	}
-	tx = tx.Offset(query.Offset).Limit(query.Limit)
+	for _, sort := range query.Sorts {
+		if sort.TableName() != mainModel.TableName() {
+			continue
+		}
+		tx = tx.Order(fmt.Sprintf("%s %s", sort.Column, sort.Direction))
+	}
+	if query.Limit.Valid {
+		tx = tx.Limit(int(query.Limit.Int64))
+	}
+	tx = tx.Offset(query.Offset)
 	return tx
 }
 
-func buildWhereScopeFromRequestQueryFilter(tx *gorm.DB, filter requests.QueryFilter) (
-	scope func(*gorm.DB) *gorm.DB) {
-	filter.Values = sliceh.Map(filter.Values, func(value any) any {
-		valueStr := fmt.Sprintf("%v", value)
-		// Parse value
-		if value, err := strconv.ParseFloat(valueStr, 10); err == nil {
-			return value
-		} else if value, err := strconv.ParseBool(valueStr); err == nil {
-			return value
-		} else if value, err := time.Parse(time.RFC3339, valueStr); err == nil {
-			return value
-		} else {
-			return valueStr
-		}
-	})
-
+// buildFromRequestQueryFilter dynamically build a query filter from the given requests.QueryFilter
+func buildFromRequestQueryFilter(tx *gorm.DB, filter requests.QueryFilter) *gorm.DB {
 	switch filter.Operator {
-	case "==", "=", "":
-		scope = func(*gorm.DB) *gorm.DB {
-			tx = tx.Where(filter.Column+" = ?", filter.Values[0])
-			// Loop start from index one not index zero
-			for i := 1; i < len(filter.Values); i++ {
-				tx = tx.Or(filter.Column+" = ?", filter.Values[i])
-			}
-			return tx
-		}
+	// Equals query
+	case enums.QueryFilterOperatorEquals:
+		tx = tx.Where(filter.Column+" = ?", filter.Values[0])
 		break
-	case "!=", "!":
-		scope = func(*gorm.DB) *gorm.DB {
-			tx = tx.Not(filter.Column+" = ?", filter.Values[0])
-			// Loop start from index one not index zero
-			for i := 1; i < len(filter.Values); i++ {
-				tx = tx.Or(filter.Column+" = ?", filter.Values[i])
-			}
-			return tx
-		}
+	case enums.QueryFilterOperatorOrEquals:
+		tx = tx.Or(filter.Column+" = ?", filter.Values[0])
 		break
-	case ">", ">=", "<", "<=":
-		scope = func(*gorm.DB) *gorm.DB {
-			return tx.Where(filter.Column+" "+filter.Operator+" ?", filter.Values[0])
-		}
+	case enums.QueryFilterOperatorNotEquals:
+		tx = tx.Not(filter.Column+" = ?", filter.Values[0])
 		break
-	case "--", "-":
-		scope = func(*gorm.DB) *gorm.DB {
-			return tx.Where(filter.Column+" BETWEEN ? AND ?",
-				filter.Values[0],
-				filter.Values[1],
-			)
-		}
+	case enums.QueryFilterOperatorOrNotEquals:
+		tx = tx.Or("NOT "+filter.Column+" = ?", filter.Values[0])
 		break
-	case ".%", "LIKE%":
-		scope = func(*gorm.DB) *gorm.DB {
-			return tx.Where(
-				filter.Column+" LIKE ?",
-				fmt.Sprintf("%v%s", filter.Values[0], "%"),
-			)
-		}
+	// Contains query
+	case enums.QueryFilterOperatorContains:
+		tx = tx.Where(filter.Column+" LIKE ?", fmt.Sprintf("%s%v%s", "%", filter.Values[0], "%"))
 		break
-	case "%.", "%LIKE":
-		scope = func(*gorm.DB) *gorm.DB {
-			return tx.Where(
-				filter.Column+" LIKE  ?",
-				fmt.Sprintf("%s%v", "%", filter.Values[0]),
-			)
-		}
+	case enums.QueryFilterOperatorOrContains:
+		tx = tx.Or(filter.Column+" LIKE ?", fmt.Sprintf("%s%v%s", "%", filter.Values[0], "%"))
 		break
-	case "%%", "%.%", "%LIKE%":
-		scope = func(*gorm.DB) *gorm.DB {
-			return tx.Where(
-				filter.Column+" LIKE ?",
-				fmt.Sprintf("%s%v%s", "%", filter.Values[0], "%"),
-			)
-		}
+	case enums.QueryFilterOperatorNotContains:
+		tx = tx.Not(filter.Column+" LIKE ?", fmt.Sprintf("%s%v%s", "%", filter.Values[0], "%"))
+		break
+	case enums.QueryFilterOperatorOrNotContains:
+		tx = tx.Or("NOT "+filter.Column+" LIKE ?", fmt.Sprintf("%s%v%s", "%", filter.Values[0], "%"))
+		break
+	// Starts with query
+	case enums.QueryFilterOperatorStartsWith:
+		tx = tx.Where(filter.Column+" LIKE ?", fmt.Sprintf("%v%s", filter.Values[0], "%"))
+		break
+	case enums.QueryFilterOperatorOrStartsWith:
+		tx = tx.Or(filter.Column+" LIKE ?", fmt.Sprintf("%v%s", filter.Values[0], "%"))
+		break
+	case enums.QueryFilterOperatorNotStartsWith:
+		tx = tx.Not(filter.Column+" LIKE ?", fmt.Sprintf("%v%s", filter.Values[0], "%"))
+		break
+	case enums.QueryFilterOperatorOrNotStartsWith:
+		tx = tx.Or("NOT "+filter.Column+" LIKE ?", fmt.Sprintf("%v%s", filter.Values[0], "%"))
+		break
+	// Ends with query
+	case enums.QueryFilterOperatorEndsWith:
+		tx = tx.Where(filter.Column+" LIKE ?", fmt.Sprintf("%s%v", "%", filter.Values[0]))
+		break
+	case enums.QueryFilterOperatorOrEndsWith:
+		tx = tx.Or(filter.Column+" LIKE ?", fmt.Sprintf("%s%v", "%", filter.Values[0]))
+		break
+	case enums.QueryFilterOperatorNotEndsWith:
+		tx = tx.Not(filter.Column+" LIKE ?", fmt.Sprintf("%s%v", "%", filter.Values[0]))
+		break
+	case enums.QueryFilterOperatorOrNotEndsWith:
+		tx = tx.Or("NOT "+filter.Column+" LIKE ?", fmt.Sprintf("%s%v", "%", filter.Values[0]))
+		break
+	// Greater than query
+	case enums.QueryFilterOperatorGreaterThan:
+		tx = tx.Where(filter.Column+" > ?", filter.Values[0])
+		break
+	case enums.QueryFilterOperatorOrGreaterThan:
+		tx = tx.Or(filter.Column+" > ?", filter.Values[0])
+		break
+	// Greater than equal query
+	case enums.QueryFilterOperatorGreaterThanEqual:
+		tx = tx.Where(filter.Column+" >= ?", filter.Values[0])
+		break
+	case enums.QueryFilterOperatorOrGreaterThanEqual:
+		tx = tx.Or(filter.Column+" >= ?", filter.Values[0])
+		break
+	// Lesser than query
+	case enums.QueryFilterOperatorLesserThan:
+		tx = tx.Where(filter.Column+" < ?", filter.Values[0])
+		break
+	case enums.QueryFilterOperatorOrLesserThan:
+		tx = tx.Or(filter.Column+" < ?", filter.Values[0])
+		break
+	// Lesser than equal query
+	case enums.QueryFilterOperatorLesserThanEqual:
+		tx = tx.Where(filter.Column+" <= ?", filter.Values[0])
+		break
+	case enums.QueryFilterOperatorOrLesserThanEqual:
+		tx = tx.Or(filter.Column+" <= ?", filter.Values[0])
+		break
+	// In array query
+	case enums.QueryFilterOperatorIn:
+		tx = tx.Where(filter.Column+" IN ?", filter.Values...)
+		break
+	case enums.QueryFilterOperatorOrIn:
+		tx = tx.Or(filter.Column+" IN ?", filter.Values...)
+		break
+	case enums.QueryFilterOperatorNotIn:
+		tx = tx.Not(filter.Column+" IN ?", filter.Values...)
+		break
+	case enums.QueryFilterOperatorOrNotIn:
+		tx = tx.Or("NOT "+filter.Column+" IN ?", filter.Values...)
+		break
+	// Between query
+	case enums.QueryFilterOperatorBetween:
+		tx = tx.Where(filter.Column+" BETWEEN ? AND ?", filter.Values[0:1]...)
+		break
+	case enums.QueryFilterOperatorOrBetween:
+		tx = tx.Or(filter.Column+" BETWEEN ? AND ?", filter.Values[0:1]...)
+		break
+	case enums.QueryFilterOperatorNotBetween:
+		tx = tx.Not(filter.Column+" BETWEEN ? AND ?", filter.Values[0:1]...)
+		break
+	case enums.QueryFilterOperatorOrNotBetween:
+		tx = tx.Or("NOT "+filter.Column+" BETWEEN ? AND ?", filter.Values[0:1]...)
 		break
 	}
-	return
+	return tx
 }
--- app/http/requests/query.go
+++ app/http/requests/query.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/arfanxn/coursefan-gofiber/app/enums"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/boolh"
@@ -13,48 +15,36 @@ import (
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/strh"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/synch"
 	"github.com/gofiber/fiber/v2"
+	"gopkg.in/guregu/null.v4"
 )
 
-type QueryFilter struct {
-	// Column
-	Column string
-	// Operator, e.g. == or != or > or >= or < or <= or %% or --
-	Operator string
-	// Values
-	Values []any
-}
+/*
+ * Query
+ */
 
-// Query represents a request query
 type Query struct {
-	// Filters is a list of where conditions
-	// query = filters=name:%arfan%;gender:male|female;age:>=18;hobby:!=swimming;created_at:2020-2023 ,or via
-	Filters []QueryFilter `json:"filters"`
-	// OrderBys determines the order of the returned items
-	// query = order_bys=name:asc;age:desc;
-	OrderBys map[string]string `json:"order_bys"`
-	// Withs determines the relation that will be loaded along the items
-	// query = withs=users.user_profiles;users.users_settings
-	Withs []string `json:"withs"`
-	// Limit limits the returned items
-	Limit int `json:"limit"`
-	// Offset skip some items and then return the items after that offset/skip
-	Offset int `json:"offset"`
-}
-
-// FromContext fills query from the given context
-func (query *Query) FromContext(c *fiber.Ctx) error {
-	errs := []error{
-		query.setFiltersFromContext(c),
-		query.setOrderBysFromContext(c),
-		query.setWithsFromContext(c),
-		query.setLimitOffsetFromContext(c),
-	}
-	for _, err := range errs {
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	// Column represents column to be included in the query
+	// querystring/formdata : column=id,name,email
+	Columns []string
+	// Inlcludes represents relations to be included in the query
+	// querystring/formdata : include[users]=id,name,email
+	Includes []QueryInclude
+	// Scopes represents query scope to be included in the query
+	// querystring/formdata : scope=hasFriends,hasEmployees
+	Scopes []string
+	// Aggregates represents aggregates to be included in the query
+	// querystring/formdata : aggregate[users]=count or aggregate[wallet.balance]=sum
+	Aggregates []QueryAggregate
+	// Sorts represents the sort order to be included in the query
+	// querystring/formdata : sort[users.created_at]=desc
+	Sorts []QuerySort
+	// Filters represents the filter to be included in the query
+	// querystring/formdata : filter[users.name][eq]=Jack
+	Filters []QueryFilter
+	// Limit represents the limit items to be returned in the query
+	Limit null.Int
+	// Offset represents the total items to be offseted in the query
+	Offset int
 }
 
 // AddFilter append the given QueryFilter to the Query.FIlters
@@ -62,149 +52,7 @@ func (query *Query) AddFilter(filters ...QueryFilter) {
 	query.Filters = append(query.Filters, filters...)
 }
 
-// setFiltersFromContext will set Query.Filters from the the given context
-func (query *Query) setFiltersFromContext(c *fiber.Ctx) (err error) {
-	queryStr := c.Query("filters")
-
-	syncronizer := synch.NewSyncronizer()
-	defer syncronizer.Close()
-	var filters []QueryFilter
-	filterStrings := strings.Split(queryStr, ";")
-	for _, filterString := range filterStrings {
-		syncronizer.WG().Add(1)
-		go func(filterString string) {
-			defer syncronizer.WG().Done()
-			expression := regexp.MustCompile("^([\\w.]+):([=!<>%]{1,2})?([`]{1}[^`]+[`]{1})([|%-]{1,2})?(`{1}[^`]+`{1})?")
-			filterArgs := expression.FindStringSubmatch(filterString)
-			if len(filterArgs) == 0 {
-				return
-			}
-
-			var filter QueryFilter
-			filter.Column = filterArgs[1]
-			filter.Operator = filterArgs[2]
-			filter.Values = sliceh.Map(
-				regexp.MustCompile("(`{1}[^`]+`{1})").FindAllString(filterString, -1),
-				func(value string) any {
-					return strings.Trim(value, "`")
-				})
-			secondOperator := filterArgs[4]
-
-			if strings.Contains(filter.Operator, "!") {
-				// if contains,then set as "not equal" operator
-				filter.Operator = "!="
-			} else if strings.Contains(filter.Operator, "%") || strings.Contains(secondOperator, "%") {
-				// if contains,then set as "like" operator
-				operator := boolh.Ternary(filter.Operator == "%", "%", ".")
-				operator += boolh.Ternary(secondOperator == "%", "%", ".")
-				filter.Operator = operator
-			} else if sliceh.Contains([]string{">", ">=", "<", "<="}, filter.Operator) {
-				// keep this empty
-			} else if strings.Contains(secondOperator, "-") {
-				// if contains,then set as "between" operator
-				filter.Operator = "--"
-			} else {
-				// Default is "equal" operator
-				filter.Operator = "=="
-			}
-			syncronizer.M().Lock()
-			filters = append(filters, filter)
-			syncronizer.M().Unlock()
-		}(filterString)
-	}
-	syncronizer.WG().Wait()
-	query.Filters = filters
-	return
-}
-
-// setOrderBysFromContentex sets Query.OrderBy from the given contenxt
-func (query *Query) setOrderBysFromContext(c *fiber.Ctx) (err error) {
-	queryStr := c.Query("order_bys")
-
-	syncronizer := synch.NewSyncronizer()
-	defer syncronizer.Close()
-	orderBys := map[string]string{}
-	orderByStrings := strings.Split(queryStr, ";")
-	for _, orderByString := range orderByStrings {
-		syncronizer.WG().Add(1)
-		go func(orderByString string) {
-			defer syncronizer.WG().Done()
-			splitted := strings.Split(orderByString, ":")
-			if len(splitted) != 2 {
-				return
-			}
-			column := splitted[0]
-			orderingType := splitted[1]
-			orderBys[column] = orderingType
-		}(orderByString)
-	}
-	syncronizer.WG().Wait()
-	query.OrderBys = orderBys
-	return
-}
-
-// setWithsFromContext will set Query.Withs from the the context
-func (query *Query) setWithsFromContext(c *fiber.Ctx) (err error) {
-	queryStr := c.Query("withs")
-	var withs []string
-	for _, with := range strings.Split(queryStr, ";") {
-		with = strh.StrToDelimetedCamel(with, ".")
-		withs = append(withs, with)
-	}
-	query.Withs = withs
-	return
-}
-
-// setLimitOffsetFromContext
-func (query *Query) setLimitOffsetFromContext(c *fiber.Ctx) (err error) {
-	query.Limit = c.QueryInt("limit", c.QueryInt("per_page", 10))
-	query.Offset = c.QueryInt(
-		"offset",
-		((c.QueryInt("page", 1) - 1) * query.Limit),
-	)
-	return
-}
-
-type QueryFilterExp struct {
-	// Column
-	Column string
-	// Operator, e.g. == or != or > or >= or < or <= or %% or --
-	Operator string
-	// Value
-	Value any
-}
-
-type QueryIncludeExp struct {
-	Column  string
-	Selects []string
-}
-
-type QuerySortExp struct {
-	Column    string
-	Direction string
-}
-
-type QueryExp struct {
-	// querystring : select=column,column2,column3
-	Selects []string
-	// querystring : include=table:column1,column2;tabl2.table3;
-	Includes []QueryIncludeExp
-	// querystring : scope=hasComment,hasLike
-	Scopes []string
-	// querystring : withCount=table,table2
-	Counts []string
-	// querystring : sort=table.column:desc,table.column:rand
-	Sorts []QuerySortExp
-	// querystring : filter[column][operator]=value
-	Filters []QueryFilterExp
-}
-
-// AddFilter append the given QueryFilter to the Query.FIlters
-func (query *QueryExp) AddFilter(filters ...QueryFilterExp) {
-	query.Filters = append(query.Filters, filters...)
-}
-
-func (input *QueryExp) FromContext(c *fiber.Ctx) (err error) {
+func (input *Query) FromContext(c *fiber.Ctx) (err error) {
 	defer func() { // incase of error by unprocessable entity
 		err = errorh.AnyToErrorOrNil(recover())
 		if err != nil {
@@ -219,65 +67,174 @@ func (input *QueryExp) FromContext(c *fiber.Ctx) (err error) {
 		return
 	}
 
-	for key, values := range url.Query() {
+	urlQueries := url.Query()
+
+	// Pagination Limit / Per Page
+	if limit, err := strconv.ParseInt(urlQueries.Get("limit"), 10, 64); err == nil {
+		input.Limit = null.NewInt(limit, true)
+	} else if perPage, err := strconv.ParseInt(urlQueries.Get("per_page"), 10, 64); err == nil {
+		input.Limit = null.NewInt(perPage, true)
+	}
+	// Pagination Offset / Page
+	if offset, err := strconv.ParseInt(urlQueries.Get("offset"), 10, 64); err == nil {
+		input.Offset = int(offset)
+	} else if page, err := strconv.ParseInt(urlQueries.Get("page"), 10, 64); err == nil {
+		input.Offset = (int(page) - 1) * int(input.Limit.Int64)
+	}
+
+	for key, values := range urlQueries {
 		syncronizer.WG().Add(1)
 		go func(key string, values []string) {
 			defer syncronizer.WG().Done()
-			if regexp.MustCompile("^selects?$").MatchString(key) {
-				for _, values := range values {
-					input.Selects = append(input.Selects, strings.Split(values, ",")...)
-				}
-			} else if regexp.MustCompile("^includes?$").MatchString(key) {
-				for _, values := range values {
-					for _, value := range strings.Split(values, ";") {
-						var (
-							splitted = strings.Split(value, ":")
-							column   = splitted[0]
-							selects  = []string{}
-						)
-						if len(splitted) > 1 && (splitted[1] != "") {
-							selects = strings.Split(splitted[1], ",")
-						}
-						input.Includes = append(input.Includes, QueryIncludeExp{
-							Column:  column,
-							Selects: selects,
-						})
+			if err := syncronizer.Err(); err != nil {
+				return
+			}
+			syncronizer.RWM().Lock()
+			defer syncronizer.RWM().Unlock()
+			if regexp.MustCompile(`^(selects?|columns?)$`).MatchString(key) {
+				// Column selection
+				input.Columns = sliceh.Merge(
+					input.Columns,
+					boolh.Ternary(len(values) != 0, strings.Split(values[0], ","), []string{"*"}),
+				)
+			} else if matcheds := regexp.MustCompile(`^(includes?\[([\w.]+)\])$`).
+				// Relation loader
+				FindStringSubmatch(key); len(matcheds) != 0 {
+				var (
+					limitKey     = matcheds[1] + "[limit]"
+					nullIntLimit = null.NewInt(0, false)
+					relation     = matcheds[2]
+				)
+				if values, ok := urlQueries[limitKey]; ok && (len(values) != 0) && (values[0] != "") {
+					if limit, err := strconv.ParseInt(values[0], 10, 64); err == nil {
+						nullIntLimit = null.NewInt(limit, true)
 					}
 				}
+				input.Includes = append(input.Includes, QueryInclude{
+					Relation: relation,
+					Columns: boolh.Ternary(
+						(len(values) != 0) && (values[0] != ""), strings.Split(values[0], ","), []string{"*"},
+					),
+					Limit: nullIntLimit,
+				})
 			} else if regexp.MustCompile("^scopes?$").MatchString(key) {
-				for _, values := range values {
-					input.Scopes = append(input.Scopes, strings.Split(values, ",")...)
-				}
-			} else if regexp.MustCompile("^counts?$").MatchString(key) {
-				for _, values := range values {
-					input.Counts = append(input.Counts, strings.Split(values, ",")...)
-				}
-			} else if regexp.MustCompile("^sorts?$").MatchString(key) {
-				for _, values := range values {
-					for _, value := range strings.Split(values, ",") {
-						splitted := strings.Split(value, ":")
-						column := splitted[0]
-						direction := splitted[1]
-						input.Sorts = append(input.Sorts, QuerySortExp{
-							Column:    column,
-							Direction: direction,
-						})
-					}
-				}
+				// Query scopes
+				input.Scopes = sliceh.Merge(input.Scopes, strings.Split(values[0], ","))
+			} else if matcheds := regexp.MustCompile(`^aggregates?\[([\w.]+)\]$`).
+				// Query Aggregates
+				FindStringSubmatch(key); len(matcheds) != 0 {
+				column := matcheds[1]
+				input.Aggregates = append(input.Aggregates, QueryAggregate{
+					Column: column,
+					Name:   values[0],
+				})
+			} else if matcheds := regexp.MustCompile(`^sorts?\[([\w.]+)\]$`).
+				// Query Sorts
+				FindStringSubmatch(key); len(matcheds) != 0 {
+				column := matcheds[1]
+				direction := values[0]
+				input.Sorts = append(input.Sorts, QuerySort{
+					Column:    column,
+					Direction: direction,
+				})
 			} else if matcheds := regexp.MustCompile(
-				fmt.Sprintf(`^filters?\[([\w.]+)\]\[(%s)\]`, strings.Join(enums.QueryFilterOperators(), "|")),
+				fmt.Sprintf(`^filters?\[([\w.]+)\]\[(%s)\]$`, strings.Join(enums.QueryFilterOperators(), "|")),
 			).FindStringSubmatch(key); len(matcheds) != 0 {
+				// Query Filter
 				column := matcheds[1]
 				operator := matcheds[2]
-				input.AddFilter(QueryFilterExp{
+				input.AddFilter(QueryFilter{
 					Column:   column,
 					Operator: operator,
-					Value:    values[0],
+					Values:   sliceh.Map(values, strh.StrToAny),
 				})
 			}
 		}(key, values)
 	}
 	syncronizer.WG().Wait()
+	if err = syncronizer.Err(); err != nil {
+		return
+	}
 
 	return
+}
+
+/*
+ * Query Filter
+ */
+
+type QueryFilter struct {
+	// Column
+	Column string
+	// Operator, e.g. == or != or > or >= or < or <= or %% or --
+	Operator string
+	// Values
+	Values []any
+}
+
+// TableName returns the table name of the QueryFilter.Column
+func (filter QueryFilter) TableName() string {
+	return strings.Split(filter.Column, ".")[0]
+}
+
+// CastValues casts data on QueryFilter.Values to the appropriate types, this function also returns the casted QueryFilter.Values
+func (filter *QueryFilter) CastValues() []any {
+	filter.Values = sliceh.Map(filter.Values, func(value any) any {
+		valueStr := strh.AnyToStr(value)
+		if value, err := strconv.ParseFloat(valueStr, 10); err == nil {
+			return value
+		} else if value, err := strconv.ParseBool(valueStr); err == nil {
+			return value
+		} else if value, err := time.Parse(time.RFC3339, valueStr); err == nil {
+			return value
+		} else {
+			return valueStr
+		}
+	})
+	return filter.Values
+}
+
+/*
+ * Query Include
+ */
+
+type QueryInclude struct {
+	// Relationble represents the table(s) will be included/joined in the query
+	Relation string
+	// Column represents column to be included in the query
+	Columns []string
+	// Limit represents the number of rows to be returned in the query
+	Limit null.Int
+}
+
+// TableNames returns the relation table names of the QueryInclude.Table
+func (include QueryInclude) TableNames() []string {
+	return strings.Split(include.Relation, ".")
+}
+
+/*
+ * Query Sort
+ */
+
+type QuerySort struct {
+	// Column represents the column being sorted
+	Column string
+	// Direction represents the sorting direction
+	Direction string
+}
+
+// TableName returns the table name of the QuerySort.Column
+func (filter QuerySort) TableName() string {
+	return strings.Split(filter.Column, ".")[0]
+}
+
+/*
+ * Query Aggregate
+ */
+
+type QueryAggregate struct {
+	// Column store the table or column being aggregated
+	Column string
+	// Name is the name of the aggregation e.g: count, sum, avg, min, max, etc.
+	Name string
 }
