ff748042ce267d3fb4ff3847521c63b18f055014
arfanxn <arfan2173@gmail.com>
2023-04-11 17:26:14 +0700

    repository with scope

--- app/repositories/course_repository.go
+++ app/repositories/course_repository.go
@@ -1,8 +1,11 @@
 package repositories
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/arfanxn/coursefan-gofiber/app/enums"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/ctxh"
 	"github.com/arfanxn/coursefan-gofiber/app/helpers/gormh"
 	"github.com/arfanxn/coursefan-gofiber/app/http/requests"
 	"github.com/arfanxn/coursefan-gofiber/app/models"
@@ -26,7 +29,42 @@ func (repository *CourseRepository) All(c *fiber.Ctx, queries ...requests.Query)
 	courses []models.Course, err error) {
 	db := repository.db
 	if len(queries) != 0 {
-		db = gormh.BuildFromRequestQuery(repository.db, models.Course{}, queries[0])
+		query := queries[0]
+		db = gormh.BuildFromRequestQuery(repository.db, models.Course{}, query)
+
+		if query.HasScope(
+			enums.CourseQueryScopeLectured,
+			enums.CourseQueryScopeParticipated,
+			enums.CourseQueryScopeCart,
+			enums.CourseQueryScopeWishlist,
+		) {
+			db = db.Joins(
+				fmt.Sprintf("JOIN %s ON %s.%s = %s.%s",
+					models.CourseUserRole{}.TableName(),
+					models.CourseUserRole{}.TableName(),
+					"course_id",
+					models.Course{}.TableName(),
+					"id",
+				))
+
+			if query.HasScope(enums.CourseQueryScopeLectured) {
+				db = db.
+					Where(models.CourseUserRole{}.TableName()+".relation", enums.CourseUserRoleRelationLecturer).
+					Where(models.CourseUserRole{}.TableName()+".user_id", ctxh.MustGetUser(c).Id)
+			} else if query.HasScope(enums.CourseQueryScopeParticipated) {
+				db = db.
+					Where(models.CourseUserRole{}.TableName()+".relation", enums.CourseUserRoleRelationParticipant).
+					Where(models.CourseUserRole{}.TableName()+".user_id", ctxh.MustGetUser(c).Id)
+			} else if query.HasScope(enums.CourseQueryScopeCart) {
+				db = db.
+					Where(models.CourseUserRole{}.TableName()+".relation", enums.CourseUserRoleRelationCart).
+					Where(models.CourseUserRole{}.TableName()+".user_id", ctxh.MustGetUser(c).Id)
+			} else if query.HasScope(enums.CourseQueryScopeWishlist) {
+				db = db.
+					Where(models.CourseUserRole{}.TableName()+".relation", enums.CourseUserRoleRelationWishlist).
+					Where(models.CourseUserRole{}.TableName()+".user_id", ctxh.MustGetUser(c).Id)
+			}
+		}
 	}
 	err = db.Find(&courses).Error
 	return
