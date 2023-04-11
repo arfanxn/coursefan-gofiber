f222f2d4283dfb4117f26ef70ff8b67acf9624f4
arfanxn <arfan2173@gmail.com>
2023-04-11 17:09:01 +0700

    make course query scope enums

---
+++ app/enums/course.go
@@ -0,0 +1,19 @@
+package enums
+
+// models.Course query scope enums
+const (
+	CourseQueryScopeLectured     string = "lecturerd"
+	CourseQueryScopeParticipated string = "participated"
+	CourseQueryScopeWishlist     string = "wishlist"
+	CourseQueryScopeInCart       string = "in_cart"
+)
+
+// CourseQueryScopes returns slice of Course query scope enums
+func CourseQueryScopes() []string {
+	return []string{
+		CourseQueryScopeLectured,
+		CourseQueryScopeParticipated,
+		CourseQueryScopeWishlist,
+		CourseQueryScopeInCart,
+	}
+}
