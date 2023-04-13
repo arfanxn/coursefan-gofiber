2a82b043a29c6e1a25514f382e1ec82ef2f23714
arfanxn <arfan2173@gmail.com>
2023-04-13 16:41:18 +0700

    update models

--- app/models/permission.go
+++ app/models/permission.go
@@ -6,3 +6,7 @@ type Permission struct {
 	Id   uuid.UUID `json:"id" gorm:"primaryKey;type:CHAR(36)"`
 	Name string    `json:"name" gorm:"index;type:VARCHAR(25) NOT NULL"`
 }
+
+func (Permission) TableName() string {
+	return "permissions"
+}
--- app/models/role.go
+++ app/models/role.go
@@ -6,3 +6,7 @@ type Role struct {
 	Id   uuid.UUID `json:"id" gorm:"primaryKey;type:CHAR(36)"`
 	Name string    `json:"name" gorm:"index;type:VARCHAR(25) NOT NULL"`
 }
+
+func (Role) TableName() string {
+	return "roles"
+}
