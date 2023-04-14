623623bef3dc2444bb3aa9288eb3f65cd8f85cb9
arfanxn <arfan2173@gmail.com>
2023-04-14 17:05:17 +0700

    update

--- app/models/lecture.go
+++ app/models/lecture.go
@@ -10,7 +10,7 @@ import (
 type Lecture struct {
 	Id            uuid.UUID    `json:"id" gorm:"primaryKey;type:char(36)"`
 	LecturePartId uuid.UUID    `json:"course_id" gorm:"type:CHAR(36);NOT NULL"`
-	LecturePart   *LecturePart `json:"course" gorm:"foreignKey:LecturePartId;constraint:OnUpdate:CASCADE;"`
+	LecturePart   *LecturePart `json:"course" gorm:"foreignKey:LecturePartId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name          string       `json:"name" gorm:"type:VARCHAR(50);NOT NULL"`
 	Order         int          `json:"order" gorm:"type:INTEGER UNSIGNED NOT NULL;default:0"`
 	CreatedAt     time.Time    `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
