5ecd3cf81b81106120b3da3328f010a141890b42
arfanxn <arfan2173@gmail.com>
2023-05-10 01:03:57 +0700

    update gorm

--- app/models/review.go
+++ app/models/review.go
@@ -12,10 +12,10 @@ type Review struct {
 	ReviewableType string      `json:"reviewable_type" gorm:"index;type:VARCHAR(25) NOT NULL"`
 	ReviewableId   uuid.UUID   `json:"reviewable_id" gorm:"type:VARCHAR(36) NOT NULL"`
 	ReviewerId     uuid.UUID   `json:"reviewer_id" gorm:"type:CHAR(36);NOT NULL"`
-	Reviewer       User        `json:"reviewer" gorm:"foreignKey:ReviewerId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Reviewer       User        `json:"reviewer" gorm:"foreignKey:ReviewerId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Rate           int         `json:"rate"  gorm:"type:TINYINT UNSIGNED NOT NULL"`
 	Title          null.String `json:"title" gorm:"type:VARCHAR(50)"`
-	Body           null.String `json:"body" gorm:"type:VARCHAR(256)"`
+	Body           null.String `json:"body" gorm:"type:TEXT"`
 	CreatedAt      time.Time   `json:"created_at" gorm:"autoCreateTime;NOT NULL"`
 	UpdatedAt      null.Time   `json:"updated_at" gorm:"autoUpdateTime"`
 }
