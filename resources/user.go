621d28bc76c03bc627cf0b632c928f27b16ad798
arfanxn <arfan2173@gmail.com>
2023-05-09 12:39:32 +0700

    add FromModel method

--- resources/user.go
+++ resources/user.go
@@ -3,6 +3,7 @@ package resources
 import (
 	"time"
 
+	"github.com/arfanxn/coursefan-gofiber/app/models"
 	"gopkg.in/guregu/null.v4"
 )
 
@@ -14,4 +15,18 @@ type User struct {
 	UpdatedAt null.Time `json:"updated_at"`
 
 	Avatar Media `json:"avatar,omitempty"`
+}
+
+func (resource *User) FromModel(model models.User) {
+	resource.Id = model.Id.String()
+	resource.Name = model.Name
+	resource.Email = model.Email
+	resource.CreatedAt = model.CreatedAt
+	resource.UpdatedAt = model.UpdatedAt
+
+	if model.Avatar != nil {
+		avatarMediaRes := Media{}
+		avatarMediaRes.FromModel(*model.Avatar)
+		resource.Avatar = avatarMediaRes
+	}
 }
