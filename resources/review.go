20b74f67b00b291618e567a411128caa1a9efcd3
arfanxn <arfan2173@gmail.com>
2023-05-09 23:11:21 +0700

    make review resource file

---
+++ resources/review.go
@@ -0,0 +1,38 @@
+package resources
+
+import (
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
+)
+
+type Review struct {
+	Id             string      `json:"id"`
+	ReviewableType string      `json:"reviewable_type"`
+	ReviewableId   string      `json:"reviewable_id"`
+	ReviewerId     string      `json:"reviewer_id"`
+	Reviewer       User        `json:"reviewer,omitempty"`
+	Rate           int         `json:"rate"`
+	Title          null.String `json:"title"`
+	Body           null.String `json:"body"`
+	CreatedAt      time.Time   `json:"created_at"`
+	UpdatedAt      null.Time   `json:"updated_at"`
+}
+
+func (resource *Review) FromModel(model models.Review) {
+	resource.Id = model.Id.String()
+	resource.ReviewableType = model.ReviewableType
+	resource.ReviewableId = model.ReviewableId.String()
+	resource.ReviewerId = model.ReviewerId.String()
+	if model.Reviewer.Id != uuid.Nil {
+		reviewerUserRes := User{}
+		reviewerUserRes.FromModel(model.Reviewer)
+		resource.Reviewer = reviewerUserRes
+	}
+	resource.Title = model.Title
+	resource.Body = model.Body
+	resource.CreatedAt = model.CreatedAt
+	resource.UpdatedAt = model.UpdatedAt
+}
