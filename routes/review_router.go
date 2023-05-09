99dcdebb76388c852bd8cf2a7005644d3d2becbc
arfanxn <arfan2173@gmail.com>
2023-05-09 22:16:34 +0700

    make review routes

---
+++ routes/review_router.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/arfanxn/coursefan-gofiber/app/providers/controllerp"
+	"github.com/arfanxn/coursefan-gofiber/app/providers/databasep"
+	"github.com/gofiber/fiber/v2"
+)
+
+// registerReviewRouter registers lecture module routes into the router
+func registerReviewRouter(router fiber.Router) {
+	reviewController := controllerp.InitReviewController(databasep.MustGetGormDB())
+	coursesIdReviews := router.Group("/courses/:course_id/reviews")
+	coursesIdReviews.Get("", reviewController.AllByCourse)
+	coursesIdReviews.Post("", reviewController.CreateByCourse)
+
+	reviewsId := router.Group("/reviews/:review_id")
+	reviewsId.Get("", reviewController.Find)
+	reviewsId.Put("", reviewController.Update)
+	reviewsId.Delete("", reviewController.Delete)
+}
