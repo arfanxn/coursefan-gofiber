2b388f8dc39333656f5837ab7919611d1e433931
arfanxn <arfan2173@gmail.com>
2023-05-11 21:23:01 +0700

    remove unused field

--- app/http/requests/review_create.go
+++ app/http/requests/review_create.go
@@ -8,7 +8,6 @@ import (
 type ReviewCreate struct {
 	ReviewableType string      `json:"reviewable_type" form:"reviewable_type" validate:"oneof=Course User Instructor"`
 	ReviewableId   string      `json:"reviewable_id" form:"reviewable_id"  validate:"required,uuid"`
-	ReviewerId     string      `json:"reviewer_id" form:"reviewer_id"  validate:"required,uuid"`
 	Rate           int         `json:"rate" validate:"required,number"`
 	Title          null.String `json:"title"`
 	Body           null.String `json:"body"`
