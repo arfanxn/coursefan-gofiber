fe48a9dae96e068438217860d4f3eca82daf516b
arfanxn <arfan2173@gmail.com>
2023-05-11 23:49:45 +0700

    make null helper

--- app/helpers/nullh/nullh.go
+++ app/helpers/nullh/nullh.go
@@ -1,1 +1,16 @@
 package nullh
+
+import (
+	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
+)
+
+func NullStringToNullUUID(nullStr null.String) (nullUUID uuid.NullUUID) {
+	parsedUUID, err := uuid.Parse(nullStr.String)
+	if (err != nil) || !nullStr.Valid {
+		return
+	}
+	nullUUID.UUID = parsedUUID
+	nullUUID.Valid = true
+	return
+}
