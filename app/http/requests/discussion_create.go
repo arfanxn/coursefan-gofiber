73b8a9180a4b62058213a85a6affb78b6b6b84ae
arfanxn <arfan2173@gmail.com>
2023-05-12 00:37:06 +0700

    update rule

--- app/http/requests/discussion_create.go
+++ app/http/requests/discussion_create.go
@@ -9,7 +9,7 @@ type DiscussionCreate struct {
 	DiscussableType     string      `json:"discussable_type" form:"discussable_type" validate:"oneof=Lecture"`
 	DiscussableId       string      `json:"discussable_id" form:"discussable_id"  validate:"required,uuid"`
 	DiscussionRepliedId null.String `json:"discussion_replied_id" form:"discussion_replied_id"`
-	Title               string      `json:"title"`
+	Title               string      `json:"title" validate:"required,min=10"`
 	Body                null.String `json:"body"`
 }
 
