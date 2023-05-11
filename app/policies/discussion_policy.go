b905828dfc296e989c2fe140bd1e2e329f34235f
arfanxn <arfan2173@gmail.com>
2023-05-11 23:55:26 +0700

    update

--- app/policies/discussion_policy.go
+++ app/policies/discussion_policy.go
@@ -39,7 +39,7 @@ func (policy *DiscussionPolicy) Find(c *fiber.Ctx, input requests.Query) (err er
 }
 
 // Create policy ensures that the user has the right permissions for create a discussion.
-func (policy *DiscussionPolicy) Create(c *fiber.Ctx, input requests.ReviewCreate) (err error) {
+func (policy *DiscussionPolicy) Create(c *fiber.Ctx, input requests.DiscussionCreate) (err error) {
 	_, err = policy.permissionRepository.FindByNameAndCUR(c, enums.PermissionNameDiscussionCreate)
 	if errorh.IsGormErrRecordNotFound(err) {
 		return fiber.ErrForbidden
@@ -50,7 +50,7 @@ func (policy *DiscussionPolicy) Create(c *fiber.Ctx, input requests.ReviewCreate
 }
 
 // CreateByLecture policy ensures that the user has the right permissions for create a discussion by the given course.
-func (policy *DiscussionPolicy) CreateByLecture(c *fiber.Ctx, input requests.ReviewCreate) (err error) {
+func (policy *DiscussionPolicy) CreateByLecture(c *fiber.Ctx, input requests.DiscussionCreate) (err error) {
 	_, err = policy.permissionRepository.FindByNameAndCUR(c, enums.PermissionNameLectureDiscussionCreate)
 	if errorh.IsGormErrRecordNotFound(err) {
 		return fiber.ErrForbidden
@@ -61,7 +61,7 @@ func (policy *DiscussionPolicy) CreateByLecture(c *fiber.Ctx, input requests.Rev
 }
 
 // Update policy ensures that the user has the right permissions for update a discussion.
-func (policy *DiscussionPolicy) Update(c *fiber.Ctx, input requests.ReviewUpdate) (err error) {
+func (policy *DiscussionPolicy) Update(c *fiber.Ctx, input requests.DiscussionUpdate) (err error) {
 	_, err = policy.permissionRepository.FindByNameAndCUR(c, enums.PermissionNameDiscussionEdit)
 	if errorh.IsGormErrRecordNotFound(err) {
 		return fiber.ErrForbidden
@@ -72,7 +72,7 @@ func (policy *DiscussionPolicy) Update(c *fiber.Ctx, input requests.ReviewUpdate
 }
 
 // Delete policy ensures that the user has the right permissions for delete a discussion.
-func (policy *DiscussionPolicy) Delete(c *fiber.Ctx, input requests.ReviewDelete) (err error) {
+func (policy *DiscussionPolicy) Delete(c *fiber.Ctx, input requests.DiscussionDelete) (err error) {
 	_, err = policy.permissionRepository.FindByNameAndCUR(c, enums.PermissionNameDiscussionDelete)
 	if errorh.IsGormErrRecordNotFound(err) {
 		return fiber.ErrForbidden
