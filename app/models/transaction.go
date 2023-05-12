8fdab16f9e9145a1adf6e0e877ad3f00edde417e
arfanxn <arfan2173@gmail.com>
2023-05-13 03:40:51 +0700

    update data type

--- app/models/transaction.go
+++ app/models/transaction.go
@@ -15,10 +15,10 @@ type Transaction struct {
 	Sender              *Wallet   `json:"sender" gorm:"foreignKey:SenderId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	ReceiverId          uuid.UUID `json:"receiver_id" gorm:"type:CHAR(36);NOT NULL"`
 	Receiver            *Wallet   `json:"receiver" gorm:"foreignKey:ReceiverId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Amount              float64   `json:"amount"  gorm:"type:BIGINT UNSIGNED NOT NULL"`
-	Rate                float64   `json:"rate"  gorm:"type:BIGINT UNSIGNED NOT NULL"`
-	Discount            float64   `json:"discount"  gorm:"type:BIGINT UNSIGNED NOT NULL"`
-	Total               float64   `json:"total"  gorm:"type:BIGINT UNSIGNED NOT NULL"`
+	Amount              float64   `json:"amount"  gorm:"type:DECIMAL(10,2) NOT NULL"`
+	Rate                float64   `json:"rate"  gorm:"type:DECIMAL(10,2) NOT NULL"`
+	Discount            float64   `json:"discount"  gorm:"type:DECIMAL(10,2) NOT NULL"`
+	Total               float64   `json:"total"  gorm:"type:DECIMAL(10,2) NOT NULL"`
 	CancelledAt         null.Time `json:"cancelled_at" gorm:"type:DATETIME(3)"`
 	ChargebackedAt      null.Time `json:"chargebacked_at" gorm:"type:DATETIME(3)"`
 	ExpiredAt           null.Time `json:"expired_at" gorm:"type:DATETIME(3)"`
