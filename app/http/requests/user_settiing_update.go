88633282a1784d46c26bd53872def4131593ccab
arfanxn <arfan2173@gmail.com>
2023-05-12 15:00:48 +0700

    add oneof rule

--- app/http/requests/user_settiing_update.go
+++ app/http/requests/user_settiing_update.go
@@ -7,7 +7,7 @@ import (
 
 type UserSettingUpdate struct {
 	UserId uuid.UUID `json:"user_id" validate:"required,uuid"`
-	Key    string    `json:"key" validate:"required"`
+	Key    string    `json:"key" validate:"required,oneof=user_public_profile user_public_course notification_accept_promotion"`
 	Value  string    `json:"value" validate:"required"`
 }
 
