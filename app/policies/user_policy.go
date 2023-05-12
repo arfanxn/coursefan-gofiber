aa66d4a405c0f5d9e75a04d574bf97b8242eb292
arfanxn <arfan2173@gmail.com>
2023-05-12 10:58:45 +0700

    update user policy

--- app/policies/user_policy.go
+++ app/policies/user_policy.go
@@ -1,6 +1,12 @@
 package policies
 
-import "github.com/arfanxn/coursefan-gofiber/app/repositories"
+import (
+	"github.com/arfanxn/coursefan-gofiber/app/enums"
+	"github.com/arfanxn/coursefan-gofiber/app/helpers/errorh"
+	"github.com/arfanxn/coursefan-gofiber/app/http/requests"
+	"github.com/arfanxn/coursefan-gofiber/app/repositories"
+	"github.com/gofiber/fiber/v2"
+)
 
 type UserPolicy struct {
 	permissionRepository *repositories.PermissionRepository
@@ -8,4 +14,15 @@ type UserPolicy struct {
 
 func NewUserPolicy(permissionRepository *repositories.PermissionRepository) *UserPolicy {
 	return &UserPolicy{permissionRepository: permissionRepository}
+}
+
+// AllByCourse policy ensures that the user has the right permissions for access a course users.
+func (policy *UserPolicy) AllByCourse(c *fiber.Ctx, input requests.Query) (err error) {
+	_, err = policy.permissionRepository.FindByNameAndCUR(c, enums.PermissionNameCourseUserView)
+	if errorh.IsGormErrRecordNotFound(err) {
+		return fiber.ErrForbidden
+	} else if err != nil {
+		return
+	}
+	return nil
 }
