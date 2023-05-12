e208d55cd84f26c55890651a7d367acd9daaf572
arfanxn <arfan2173@gmail.com>
2023-05-12 22:15:46 +0700

    make wallet resource

---
+++ resources/wallet.go
@@ -0,0 +1,30 @@
+package resources
+
+import (
+	"time"
+
+	"github.com/arfanxn/coursefan-gofiber/app/models"
+	"gopkg.in/guregu/null.v4"
+)
+
+type Wallet struct {
+	Id        string    `json:"id"`
+	OwnerId   string    `json:"user_id"`
+	Owner     *User     `json:"user,omitempty"`
+	Balance   int64     `json:"balance" `
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt null.Time `json:"updated_at"`
+}
+
+func (resource *Wallet) FromModel(model models.Wallet) {
+	resource.Id = model.Id.String()
+	resource.OwnerId = model.OwnerId.String()
+	if model.Owner != nil {
+		userRes := User{}
+		userRes.FromModel(*model.Owner)
+		resource.Owner = &userRes
+	}
+	resource.Balance = model.Balance
+	resource.CreatedAt = model.CreatedAt
+	resource.UpdatedAt = model.UpdatedAt
+}
