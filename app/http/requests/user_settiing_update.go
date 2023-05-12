f5b833c9adfddd06efd6f1dfe36b6494cebbae8d
arfanxn <arfan2173@gmail.com>
2023-05-12 14:16:56 +0700

    make user setting request

---
+++ app/http/requests/user_settiing_update.go
@@ -0,0 +1,23 @@
+package requests
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type UserSettingUpdate struct {
+	UserId uuid.UUID `json:"user_id" validate:"required,uuid"`
+	Key    string    `json:"key" validate:"required"`
+	Value  string    `json:"value" validate:"required"`
+}
+
+// FromContext fills input from the given context
+func (input *UserSettingUpdate) FromContext(c *fiber.Ctx) (err error) {
+	err = c.BodyParser(input)
+
+	if settingKey := c.Params("setting_key"); settingKey != "" {
+		input.Key = settingKey
+	}
+
+	return
+}
