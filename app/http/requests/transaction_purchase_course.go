9451bf1879ca7c818c0f2011db8486d6b5b62160
arfanxn <arfan2173@gmail.com>
2023-05-13 03:25:36 +0700

    update from context method

--- app/http/requests/transaction_purchase_course.go
+++ app/http/requests/transaction_purchase_course.go
@@ -2,16 +2,20 @@ package requests
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
 )
 
 type TransactionPurchaseCourse struct {
-	TransactionableId uuid.UUID `json:"transactionable_id" form:"transactionable_id" validate:"required,uuid"`
-	SenderId          uuid.UUID `json:"sender_id" form:"sender_id" validate:"required,uuid"`
+	TransactionableId string `json:"transactionable_id" form:"transactionable_id" validate:"required,uuid"`
+	SenderId          string `json:"sender_id" form:"sender_id" validate:"required,uuid"`
 }
 
 // FromContext fills input from the given context
 func (input *TransactionPurchaseCourse) FromContext(c *fiber.Ctx) (err error) {
 	err = c.BodyParser(input)
+
+	if courseId := c.Params("course_id"); courseId != "" {
+		input.TransactionableId = courseId
+	}
+
 	return
 }
