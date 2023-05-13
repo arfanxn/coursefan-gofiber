34cd3f0f3b2c6dd6f47368b79399406c954e1a26
arfanxn <arfan2173@gmail.com>
2023-05-13 21:57:52 +0700

    fix parse error

--- app/http/requests/midtrans_notification.go
+++ app/http/requests/midtrans_notification.go
@@ -1,6 +1,9 @@
 package requests
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gofiber/fiber/v2"
+)
 
 type MidtransNotification struct {
 	VANumbers         []MidtransNotificationVANumbers
@@ -8,14 +11,14 @@ type MidtransNotification struct {
 	TransactionTime   string  `json:"transaction_time,omitempty" form:"transaction_time"`
 	TransactionStatus string  `json:"transaction_status,omitempty" form:"transaction_status"`
 	StatusMessage     string  `json:"status_message,omitempty" form:"status_message"`
-	StatusCode        int     `json:"status_code,omitempty" form:"status_code"`
+	StatusCode        int     `json:"status_code,string,omitempty" form:"-" validate:"number"`
 	SignatureKey      string  `json:"signature_key,omitempty" form:"signature_key"`
 	PaymentType       string  `json:"payment_type,omitempty" form:"payment_type"`
 	PaymentAmounts    []any   `json:"payment_amounts,omitempty" form:"payment_amounts"`
 	OrderId           string  `json:"order_id,omitempty" form:"order_id"`
 	MerchantId        string  `json:"merchant_id,omitempty" form:"merchant_id"`
-	GrossAmount       float64 `json:"gross_amount,omitempty" form:"gross_amount"`
-	FraudStatus       float64 `json:"fraud_status,omitempty" form:"fraud_status"`
+	GrossAmount       float64 `json:"gross_amount,string,omitempty" form:"-" validate:"number"`
+	FraudStatus       string  `json:"fraud_status,omitempty" form:"fraud_status"`
 	ExpiryTime        string  `json:"expiry_time,omitempty" form:"expiry_time"`
 	Currency          string  `json:"currency,omitempty" form:"currency"`
 }
@@ -27,6 +30,8 @@ type MidtransNotificationVANumbers struct {
 
 // FromContext fills input from the given context
 func (input *MidtransNotification) FromContext(c *fiber.Ctx) (err error) {
+	spew.Dump("Ctx:", string(c.Body()))
+
 	err = c.BodyParser(input)
 
 	return
