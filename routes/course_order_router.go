7e7fdcb5d1f77a2dc713e32c27f912e0720cbf06
arfanxn <arfan2173@gmail.com>
2023-05-13 20:06:00 +0700

    update path

--- routes/course_order_router.go
+++ routes/course_order_router.go
@@ -9,9 +9,10 @@ import (
 // registerCourseOrderRouter registers courseOrder module routes into the router
 func registerCourseOrderRouter(router fiber.Router) {
 	courseOrderController := controllerp.InitCourseOrderController(databasep.MustGetGormDB())
-	router.Get("/users/self/course_orders", courseOrderController.AllByAuthUser)
+	router.Get("/users/self/course-orders", courseOrderController.AllByAuthUser)
 
-	courseOrders := router.Group("/course_orders")
-	courseOrders.Get("/:course_order_id", courseOrderController.Find)
+	courseOrders := router.Group("/course-orders")
+	courseOrdersId := courseOrders.Group("/:course_order_id")
+	courseOrdersId.Get("", courseOrderController.Find)
 	courseOrders.Post("", courseOrderController.CreateByAuthUser)
 }
