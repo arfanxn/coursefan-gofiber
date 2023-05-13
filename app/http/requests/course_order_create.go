bfd2d9ac4ec127f014cc73dde9ec38e424efaff2
arfanxn <arfan2173@gmail.com>
2023-05-13 13:06:59 +0700

    make CourseOrderCreate req file

---
+++ app/http/requests/course_order_create.go
@@ -0,0 +1,21 @@
+package requests
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type CourseOrderCreate struct {
+	UserId   string `json:"user_id" form:"user_id" validate:"required,uuid"`
+	CourseId string `json:"course_id" form:"course_id" validate:"required,uuid"`
+}
+
+// FromContext fills input from the given context
+func (input *CourseOrderCreate) FromContext(c *fiber.Ctx) (err error) {
+	err = c.BodyParser(input)
+
+	if courseId := c.Params("course_id"); courseId != "" {
+		input.CourseId = courseId
+	}
+
+	return
+}
